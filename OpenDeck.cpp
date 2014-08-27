ee280e0f3c43bef9a0b97df4c7e9cbd80fd9f5f9
paradajz <2544094+paradajz@users.noreply.github.com>
2014-08-27 09:35:41 +0200

    formatting

--- OpenDeck.cpp
+++ OpenDeck.cpp
@@ -1,7 +1,7 @@
 /*
 
 OpenDeck MIDI controller firmware v1.93
-Last revision date: 2014-08-26
+Last revision date: 2014-08-27
 Author: Igor Petrovic
 
 */
@@ -12,91 +12,91 @@ Author: Igor Petrovic
 #include <avr/io.h>
 
 //velocity for on and off events
-#define MIDI_NOTE_ON_VELOCITY	127
-#define MIDI_NOTE_OFF_VELOCITY	0
+#define MIDI_NOTE_ON_VELOCITY   127
+#define MIDI_NOTE_OFF_VELOCITY  0
 
 //define time after which code in timedLoop function runs
-#define COLUMN_SWITCH_TIME	1
+#define COLUMN_SWITCH_TIME  1
 
 
 //hardware control
 
 //initialize pins
-void initPins()	{
+void initPins() {
 
-	//set in/out pins
-	DDRD = 0x02;
-	DDRB = 0x0F;
-	DDRC = 0x3F;
+    //set in/out pins
+    DDRD = 0x02;
+    DDRB = 0x0F;
+    DDRC = 0x3F;
 
-	//enable internal pull-up resistors for button rows
-	PORTD = 0xFC;
+    //enable internal pull-up resistors for button rows
+    PORTD = 0xFC;
 
-	//select first column
-	PORTC = 0x00;
+    //select first column
+    PORTC = 0x00;
 
 }
 
 //switch to next matrix column
-void activateColumn(uint8_t column)	{
+void activateColumn(uint8_t column) {
 
-	//column switching is controlled by 74HC238 decoder
-	PORTC &= 0xC7;
-	PORTC |= (0xC7 | (column << 3));
+    //column switching is controlled by 74HC238 decoder
+    PORTC &= 0xC7;
+    PORTC |= (0xC7 | (column << 3));
 
 }
 
 //turn led row on
-void ledRowOn(uint8_t rowNumber)	{
+void ledRowOn(uint8_t rowNumber)    {
 
-	switch (rowNumber)	{
+    switch (rowNumber)  {
 
-		case 0:
-		//turn on first LED row
-		PORTB |= 0x01;
-		break;
+        case 0:
+        //turn on first LED row
+        PORTB |= 0x01;
+        break;
 
-		case 1:
-		//turn on second LED row
-		PORTB |= 0x02;
-		break;
+        case 1:
+        //turn on second LED row
+        PORTB |= 0x02;
+        break;
 
-		case 2:
-		//turn on third LED row
-		PORTB |= 0x04;
-		break;
+        case 2:
+        //turn on third LED row
+        PORTB |= 0x04;
+        break;
 
-		case 3:
-		//turn on fourth LED row
-		PORTB |= 0x08;
-		break;
+        case 3:
+        //turn on fourth LED row
+        PORTB |= 0x08;
+        break;
 
-		default:
-		break;
+        default:
+        break;
 
-	}
+    }
 
 }
 
 //turn all LED rows off
-void ledRowsOff()	{
+void ledRowsOff()   {
 
-	PORTB &= 0xF0;
+    PORTB &= 0xF0;
 
 }
 
 //control select pins on mux
-void setMuxOutput(uint8_t muxInput)	{
+void setMuxOutput(uint8_t muxInput) {
 
-	PORTC &= 0xF8;
-	PORTC |= muxInput;
+    PORTC &= 0xF8;
+    PORTC |= muxInput;
 
 }
 
 //get button readings from all rows in matrix
-void readButtons(uint8_t &buttonColumnState)	{
+void readButtons(uint8_t &buttonColumnState)    {
 
-	buttonColumnState = ((PIND >> 4) & 0x0F);
+    buttonColumnState = ((PIND >> 4) & 0x0F);
 
 }
 
@@ -105,15 +105,15 @@ void readButtons(uint8_t &buttonColumnState)	{
 //MIDI callback handlers
 
 //receive and store note on data
-void getNoteOnData(uint8_t channel, uint8_t pitch, uint8_t velocity)	{
+void getNoteOnData(uint8_t channel, uint8_t pitch, uint8_t velocity)    {
 
-	openDeck.storeReceivedNote(channel, pitch, velocity);
+    openDeck.storeReceivedNote(channel, pitch, velocity);
 
 }
 
-void getSysExData(uint8_t *sysExArray, uint8_t size)	{
+void getSysExData(uint8_t *sysExArray, uint8_t size)    {
 
-	openDeck.processSysEx(sysExArray, size);
+    openDeck.processSysEx(sysExArray, size);
 
 }
 
@@ -121,52 +121,52 @@ void getSysExData(uint8_t *sysExArray, uint8_t size)	{
 //OpenDeck callback handlers
 
 //send button note midi data
-void sendButtonData(uint8_t buttonNumber, bool buttonState, uint8_t channel)	{
+void sendButtonData(uint8_t buttonNumber, bool buttonState, uint8_t channel)    {
 
-	//see checkButton in OpenDeck.cpp
+    //see checkButton in OpenDeck.cpp
 
-	switch (buttonState)	{
+    switch (buttonState)    {
 
-		case false:
-		//button released
-		if (openDeck.standardNoteOffEnabled())	MIDI.sendNoteOff(buttonNumber, MIDI_NOTE_OFF_VELOCITY, channel);
-		else									MIDI.sendNoteOn(buttonNumber, MIDI_NOTE_OFF_VELOCITY, channel);
-		break;
+        case false:
+        //button released
+        if (openDeck.standardNoteOffEnabled())  MIDI.sendNoteOff(buttonNumber, MIDI_NOTE_OFF_VELOCITY, channel);
+        else                                    MIDI.sendNoteOn(buttonNumber, MIDI_NOTE_OFF_VELOCITY, channel);
+        break;
 
-		case true:
-		//button pressed
-		MIDI.sendNoteOn(buttonNumber, MIDI_NOTE_ON_VELOCITY, channel);
-		break;
+        case true:
+        //button pressed
+        MIDI.sendNoteOn(buttonNumber, MIDI_NOTE_ON_VELOCITY, channel);
+        break;
 
-	}
+    }
 
 }
 
 //send pot CC midi data
-void sendPotCCData(uint8_t potNumber, uint8_t ccValue, uint8_t channel)	{
+void sendPotCCData(uint8_t potNumber, uint8_t ccValue, uint8_t channel) {
 
-	MIDI.sendControlChange(potNumber, ccValue, channel);
+    MIDI.sendControlChange(potNumber, ccValue, channel);
 
 }
 
 //send pot note on midi data
-void sendPotNoteOnData(uint8_t note, uint8_t potNumber, uint8_t channel)	{
+void sendPotNoteOnData(uint8_t note, uint8_t potNumber, uint8_t channel)    {
 
-	MIDI.sendNoteOn(note, MIDI_NOTE_ON_VELOCITY, channel);
+    MIDI.sendNoteOn(note, MIDI_NOTE_ON_VELOCITY, channel);
 
 }
 
 //send pot note off midi data
-void sendPotNoteOffData(uint8_t note, uint8_t potNumber, uint8_t channel)	{
+void sendPotNoteOffData(uint8_t note, uint8_t potNumber, uint8_t channel)   {
 
-	if (openDeck.standardNoteOffEnabled())	MIDI.sendNoteOff(note, MIDI_NOTE_OFF_VELOCITY, channel);
-	else									MIDI.sendNoteOn(note, MIDI_NOTE_OFF_VELOCITY, channel);
+    if (openDeck.standardNoteOffEnabled())  MIDI.sendNoteOff(note, MIDI_NOTE_OFF_VELOCITY, channel);
+    else                                    MIDI.sendNoteOn(note, MIDI_NOTE_OFF_VELOCITY, channel);
 
 }
 
-void sendSysExData(uint8_t *sysExArray, uint8_t size)	{
+void sendSysExData(uint8_t *sysExArray, uint8_t size)   {
 
-	MIDI.sendSysEx(size, sysExArray, false);
+    MIDI.sendSysEx(size, sysExArray, false);
 
 }
 
@@ -174,103 +174,103 @@ void sendSysExData(uint8_t *sysExArray, uint8_t size)	{
 //start-up animation
 void startUpRoutine()  {
 
-	openDeck.oneByOneLED(true, true, true);
-	openDeck.oneByOneLED(false, false, true);
-	openDeck.oneByOneLED(true, false, false);
-	openDeck.oneByOneLED(false, true, true);
-	openDeck.oneByOneLED(true, false, true);
-	openDeck.oneByOneLED(false, false, false);
-	openDeck.allLEDsOff();
+    openDeck.oneByOneLED(true, true, true);
+    openDeck.oneByOneLED(false, false, true);
+    openDeck.oneByOneLED(true, false, false);
+    openDeck.oneByOneLED(false, true, true);
+    openDeck.oneByOneLED(true, false, true);
+    openDeck.oneByOneLED(false, false, false);
+    openDeck.allLEDsOff();
 
 }
 
 
 
 //configure opendeck library
-void setOpenDeckHandlers()	{
+void setOpenDeckHandlers()  {
 
-	openDeck.setNumberOfMux(2);
-	openDeck.setNumberOfColumns(8);
-	openDeck.setNumberOfButtonRows(4);
-	openDeck.setNumberOfLEDrows(4);
+    openDeck.setNumberOfMux(2);
+    openDeck.setNumberOfColumns(8);
+    openDeck.setNumberOfButtonRows(4);
+    openDeck.setNumberOfLEDrows(4);
 
-	openDeck.enableAnalogueInput(6);
-	openDeck.enableAnalogueInput(7);
+    openDeck.enableAnalogueInput(6);
+    openDeck.enableAnalogueInput(7);
 
-	openDeck.setHandleButtonSend(sendButtonData);
+    openDeck.setHandleButtonSend(sendButtonData);
 
-	openDeck.setHandlePotCC(sendPotCCData);
-	openDeck.setHandlePotNoteOn(sendPotNoteOnData);
-	openDeck.setHandlePotNoteOff(sendPotNoteOffData);
+    openDeck.setHandlePotCC(sendPotCCData);
+    openDeck.setHandlePotNoteOn(sendPotNoteOnData);
+    openDeck.setHandlePotNoteOff(sendPotNoteOffData);
 
-	openDeck.setHandleSysExSend(sendSysExData);
+    openDeck.setHandleSysExSend(sendSysExData);
 
-	openDeck.setHandlePinInit(initPins);
-	openDeck.setHandleColumnSwitch(activateColumn);
-	openDeck.setHandleLEDrowOn(ledRowOn);
-	openDeck.setHandleLEDrowsOff(ledRowsOff);
-	openDeck.setHandleMuxOutput(setMuxOutput);
-	openDeck.setHandleButtonRead(readButtons);
+    openDeck.setHandlePinInit(initPins);
+    openDeck.setHandleColumnSwitch(activateColumn);
+    openDeck.setHandleLEDrowOn(ledRowOn);
+    openDeck.setHandleLEDrowsOff(ledRowsOff);
+    openDeck.setHandleMuxOutput(setMuxOutput);
+    openDeck.setHandleButtonRead(readButtons);
 
 }
 
 //set MIDI handlers
-void setMIDIhandlers()	{
+void setMIDIhandlers()  {
 
-	MIDI.setHandleNoteOn(getNoteOnData);
-	MIDI.setHandleSystemExclusive(getSysExData);
+    MIDI.setHandleNoteOn(getNoteOnData);
+    MIDI.setHandleSystemExclusive(getSysExData);
 
 }
 
 
 //main
-void setup()	{
+void setup()    {
 
-	//set time in ms after which timedLoop is run
-	setTimedLoop(COLUMN_SWITCH_TIME);
+    //set time in ms after which timedLoop is run
+    setTimedLoop(COLUMN_SWITCH_TIME);
 
-	setOpenDeckHandlers();
-	setMIDIhandlers();
+    setOpenDeckHandlers();
+    setMIDIhandlers();
 
-	//write default controller settings to EEPROM
-	//openDeck.setDefaultConf();
+    //write default controller settings to EEPROM
+    //openDeck.setDefaultConf();
 
-	//initialize openDeck library
-	openDeck.init();
+    //initialize openDeck library
+    openDeck.init();
 
-	//run LED animation on start-up
-	startUpRoutine();
+    //run LED animation on start-up
+    startUpRoutine();
 
-	//read incoming MIDI messages on specified channel
-	MIDI.begin(openDeck.getInputMIDIchannel());
+    //read incoming MIDI messages on specified channel
+    MIDI.begin(openDeck.getInputMIDIchannel());
 
-	Serial.begin(38400);
-
-}
-
-void timedLoop()	{
-
-	//activate next column
-	openDeck.nextColumn();
-
-	//if any of the LEDs on current
-	//column are active, turn them on
-	if (openDeck.ledsEnabled())		openDeck.checkLEDs();
-
-	//check buttons on current column
-	if (openDeck.buttonsEnabled())	openDeck.readButtons();
+    Serial.begin(38400);
 
 }
 
-void loop()	{
+void timedLoop()    {
 
-	//constantly check for incoming MIDI messages
-	MIDI.read();
+    //activate next column
+    openDeck.nextColumn();
 
-	//check if there is any received note to process
-	openDeck.checkReceivedNote();
+    //if any of the LEDs on current
+    //column are active, turn them on
+    if (openDeck.ledsEnabled())     openDeck.checkLEDs();
 
-	//read all pots
-	if (openDeck.potsEnabled())		openDeck.readPots();
+    //check buttons on current column
+    if (openDeck.buttonsEnabled())  openDeck.readButtons();
+
+}
+
+void loop() {
+
+    //constantly check for incoming MIDI messages
+    MIDI.read();
+
+    //check if there is any received note to process
+    openDeck.checkReceivedNote();
+
+    //read all pots
+    if (openDeck.potsEnabled())     openDeck.readPots();
 
 }
--- lib/OpenDeck/SysEx.h
+++ lib/OpenDeck/SysEx.h
@@ -2,7 +2,7 @@
 
 OpenDECK library v1.93
 File: SysEx.h
-Last revision date: 2014-08-26
+Last revision date: 2014-08-27
 Author: Igor Petrovic
 
 */
@@ -13,63 +13,63 @@ Author: Igor Petrovic
 
 //sysex
 
-#define SYS_EX_MANUFACTURER_ID_0				0x00
-#define SYS_EX_MANUFACTURER_ID_1				0x53
-#define SYS_EX_MANUFACTURER_ID_2				0x43
+#define SYS_EX_MANUFACTURER_ID_0                0x00
+#define SYS_EX_MANUFACTURER_ID_1                0x53
+#define SYS_EX_MANUFACTURER_ID_2                0x43
 
-#define SYS_EX_MIDI_CHANNEL_START				0x4D
+#define SYS_EX_MIDI_CHANNEL_START               0x4D
 
-#define SYS_EX_HW_PARAMETER_START				0x54
-#define SYS_EX_SW_FEATURE_START					0x53
-#define SYS_EX_HW_FEATURE_START					0x48
+#define SYS_EX_HW_PARAMETER_START               0x54
+#define SYS_EX_SW_FEATURE_START                 0x53
+#define SYS_EX_HW_FEATURE_START                 0x48
 
-#define SYS_EX_BUTTON_START						0x42
-#define SYS_EX_POT_START						0x50
-#define SYS_EX_ENC_START						0x45
-#define SYS_EX_LED_START						0x4C
+#define SYS_EX_BUTTON_START                     0x42
+#define SYS_EX_POT_START                        0x50
+#define SYS_EX_ENC_START                        0x45
+#define SYS_EX_LED_START                        0x4C
 
-#define SYS_EX_GET								0x00
-#define SYS_EX_SET								0x01
-#define SYS_EX_ENABLE							0x01
-#define SYS_EX_DISABLE							0x00
+#define SYS_EX_GET                              0x00
+#define SYS_EX_SET                              0x01
+#define SYS_EX_ENABLE                           0x01
+#define SYS_EX_DISABLE                          0x00
 
-#define SYS_EX_GET_SET_SINGLE					0x00
-#define SYS_EX_GET_SET_ALL						0x01
+#define SYS_EX_GET_SET_SINGLE                   0x00
+#define SYS_EX_GET_SET_ALL                      0x01
 
-#define SYS_EX_GET_SET_BUTTON_TYPE				0x00
-#define SYS_EX_GET_SET_BUTTON_NOTE				0x01
+#define SYS_EX_GET_SET_BUTTON_TYPE              0x00
+#define SYS_EX_GET_SET_BUTTON_NOTE              0x01
 
-#define SYS_EX_GET_SET_POT_ENABLED				0x00
-#define SYS_EX_GET_SET_POT_INVERTED				0x01
-#define SYS_EX_GET_SET_POT_CC_NUMBER			0x02
+#define SYS_EX_GET_SET_POT_ENABLED              0x00
+#define SYS_EX_GET_SET_POT_INVERTED             0x01
+#define SYS_EX_GET_SET_POT_CC_NUMBER            0x02
 
-#define SYS_EX_GET_SET_LED_ID					0x00
+#define SYS_EX_GET_SET_LED_ID                   0x00
 
-#define SYS_EX_MC_BUTTON_NOTE					0x00
-#define SYS_EX_MC_LONG_PRESS_BUTTON_NOTE		0x01
-#define SYS_EX_MC_POT_CC						0x02
-#define SYS_EX_MC_ENC_CC						0x03
-#define SYS_EX_MC_INPUT							0x04
+#define SYS_EX_MC_BUTTON_NOTE                   0x00
+#define SYS_EX_MC_LONG_PRESS_BUTTON_NOTE        0x01
+#define SYS_EX_MC_POT_CC                        0x02
+#define SYS_EX_MC_ENC_CC                        0x03
+#define SYS_EX_MC_INPUT                         0x04
 
-#define SYS_EX_HW_P_LONG_PRESS_TIME				0x00
-#define SYS_EX_HW_P_BLINK_TIME					0x01
-#define SYS_EX_HW_P_START_UP_SWITCH_TIME		0x02
+#define SYS_EX_HW_P_LONG_PRESS_TIME             0x00
+#define SYS_EX_HW_P_BLINK_TIME                  0x01
+#define SYS_EX_HW_P_START_UP_SWITCH_TIME        0x02
 
-#define SYS_EX_SW_F_RUNNING_STATUS				0x00
-#define SYS_EX_SW_F_STANDARD_NOTE_OFF			0x01
-#define SYS_EX_SW_F_ENC_NOTES					0x02
-#define SYS_EX_SW_F_POT_NOTES					0x03
-#define SYS_EX_SW_F_LONG_PRESS					0x04
-#define SYS_EX_SW_F_LED_BLINK					0x05
-#define SYS_EX_SW_F_START_UP_ROUTINE			0x06
+#define SYS_EX_SW_F_RUNNING_STATUS              0x00
+#define SYS_EX_SW_F_STANDARD_NOTE_OFF           0x01
+#define SYS_EX_SW_F_ENC_NOTES                   0x02
+#define SYS_EX_SW_F_POT_NOTES                   0x03
+#define SYS_EX_SW_F_LONG_PRESS                  0x04
+#define SYS_EX_SW_F_LED_BLINK                   0x05
+#define SYS_EX_SW_F_START_UP_ROUTINE            0x06
 
-#define SYS_EX_HW_F_BUTTONS						0x00
-#define SYS_EX_HW_F_POTS						0x01
-#define SYS_EX_HW_F_ENC							0x02
-#define SYS_EX_HW_F_LEDS						0x03
+#define SYS_EX_HW_F_BUTTONS                     0x00
+#define SYS_EX_HW_F_POTS                        0x01
+#define SYS_EX_HW_F_ENC                         0x02
+#define SYS_EX_HW_F_LEDS                        0x03
 
-#define SYS_EX_MIN_MESSAGE_LENGHT				0x09
-#define SYS_EX_ACK								0x41
-#define SYS_EX_ERROR							0x46
+#define SYS_EX_MIN_MESSAGE_LENGHT               0x09
+#define SYS_EX_ACK                              0x41
+#define SYS_EX_ERROR                            0x46
 
 #endif /* SYSEX_H_ */
