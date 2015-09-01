62bb2a64b3d7a6f2df2cb83fa40b62c28ecd33a3
paradajz <ig.petrovi@outlook.com>
2015-09-01 22:06:22 +0200

    set 5V AREF on v1 board, function rename
    * set 5V AREF on v1 board
    * rename analogReadOwnduino to analogReadBlocking

--- OpenDeck_MCU/ADC.h
+++ OpenDeck_MCU/ADC.h
@@ -6,11 +6,12 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <avr/sfr_defs.h>
+#include "Board.h"
 
 void setUpADC();
 void set8bitADC();
 void setADCprescaler(uint8_t);
-int16_t analogReadOwnduino(uint8_t);
+int16_t analogReadBlocking(uint8_t);
 void setADCchannel(uint8_t);
 int16_t getADCvalue();
 void disconnectDigitalInADC(uint8_t);
