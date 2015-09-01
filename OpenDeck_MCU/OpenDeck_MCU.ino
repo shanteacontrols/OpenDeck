c722ac118ad5911ff79cea993f7376c86c4b6c4a
paradajz <ig.petrovi@outlook.com>
2015-09-01 22:01:17 +0200

    alternative init of v1 board

--- OpenDeck_MCU/OpenDeck_MCU.ino
+++ OpenDeck_MCU/OpenDeck_MCU.ino
@@ -8,6 +8,34 @@ Author: Igor Petrovic
 
 #include "OpenDeck.h"
 
+#ifdef BOARD_OPENDECK_1
+
+//atmega328
+//arduino adds some unnecessary stuff in setup/loop
+
+int main()  {
+
+    openDeck.init();
+
+    while(1)    {
+
+        //check incoming MIDI messages
+        openDeck.checkMIDIIn();
+
+        //check buttons
+        openDeck.readButtons();
+
+        //check analog inputs
+        openDeck.readAnalog();
+
+        //check encoders
+        openDeck.readEncoders();
+
+    }   return 0;
+
+}
+#else
+
 void setup()    {
 
     //setup
@@ -34,3 +62,4 @@ void loop() {
     #endif
 
 }
+#endif
