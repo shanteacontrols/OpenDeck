c6d01c79cb2bc77b2a8ce77bb89d39faa88d8d39
paradajz <2544094+paradajz@users.noreply.github.com>
2015-12-08 10:35:27 +0100

    match serial buffer size with sysex array size

--- src/OpenDeck_AVRC/hardware/uart/UARTsettings.h
+++ src/OpenDeck_AVRC/hardware/uart/UARTsettings.h
@@ -1,7 +1,9 @@
 #ifndef UARTSETTINGS_H_
 #define UARTSETTINGS_H_
 
+#include "..\midi/MIDIsettings.h"
+
 //RX/TX buffer size in bytes
-#define SERIAL_BUFFER_SIZE 32
+#define SERIAL_BUFFER_SIZE MIDI_SYSEX_ARRAY_SIZE
 
 #endif
