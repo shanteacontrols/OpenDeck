1696295e6e0a7346c8989aff1db65bec95d7460b
paradajz <2544094+paradajz@users.noreply.github.com>
2015-12-12 16:32:06 +0100

    remove callback handlers

--- src/OpenDeck_AVRC/midi/MIDI.h
+++ src/OpenDeck_AVRC/midi/MIDI.h
@@ -12,8 +12,6 @@ class MIDI {
     void checkInput();
     uint8_t getParameter(uint8_t messageType, uint8_t parameterID);
     bool setParameter(uint8_t messageType, uint8_t parameterID, uint8_t newParameterID);
-    void setHandleSysEx(void(*fptr)(uint8_t sysExArray[], uint8_t arraySize));
-    void setHandleNote(void(*fptr)(uint8_t note, uint8_t noteVelocity));
 
     void sendMIDInote(uint8_t, bool, uint8_t);
     void sendProgramChange(uint8_t program);
@@ -28,8 +26,6 @@ class MIDI {
     uint8_t getMIDIchannel(uint8_t);
     bool setMIDIchannel(uint8_t, uint8_t);
     bool getFeature(uint8_t featureID);
-    void (*sendSysExCallback)(uint8_t sysExArray[], uint8_t arraySize);
-    void (*sendNoteCallback)(uint8_t note, uint8_t noteVelocity);
 
 };
 
