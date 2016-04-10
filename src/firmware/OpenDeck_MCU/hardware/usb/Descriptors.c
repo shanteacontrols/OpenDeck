666b3485834c892bfcd49d426c96eafb61b3c50b
paradajz <2544094+paradajz@users.noreply.github.com>
2016-04-10 17:39:05 +0200

    handle usb task via interrupt, change device name

--- src/firmware/OpenDeck_MCU/hardware/usb/Descriptors.c
+++ src/firmware/OpenDeck_MCU/hardware/usb/Descriptors.c
@@ -260,7 +260,7 @@ const USB_Descriptor_String_t PROGMEM ManufacturerString = USB_STRING_DESCRIPTOR
  *  and is read out upon request by the host when the appropriate string ID is requested, listed in the Device
  *  Descriptor.
  */
-const USB_Descriptor_String_t PROGMEM ProductString = USB_STRING_DESCRIPTOR(L"OpenDeck_LUFA");
+const USB_Descriptor_String_t PROGMEM ProductString = USB_STRING_DESCRIPTOR(L"OpenDeck");
 
 /** This function is called by the library when in device mode, and must be overridden (see library "USB Descriptors"
  *  documentation) by the application code so that the address and size of a requested descriptor can be given
