c0a91b3ac10ab4a744167e64a516dda2b52553e2
paradajz <ig.petrovi@outlook.com>
2016-08-07 23:17:21 +0200

    versioning fixes

--- src/Firmware/version/Firmware.cpp
+++ src/Firmware/version/Firmware.cpp
@@ -2,31 +2,29 @@
 #include <avr/eeprom.h>
 #include <avr/pgmspace.h>
 
-#define VERSION_POINT_LOCATION  (FLASH_SIZE - 8)
-#define CRC_LOCATION_FLASH      (FLASH_SIZE - 2)
+#define VERSION_POINT_LOCATION  0x67F6
+#define CRC_LOCATION_FLASH      0x67FE
 #define CRC_LOCATION_EEPROM     (EEPROM_SIZE - 2)   //write crc to last eeprom location
 
-#define PGM_READ(address) ((FLASH_SIZE > 65535) ? pgm_read_word_far(address) : pgm_read_word_near(address))
+bool checkNewRevision() {
 
-//bool checkNewRevision() {
-//
-    ////current app crc is written to last flash location
-    ////previous crc is stored into eeprom
-    ////if two differ, app has changed
-//
-    //uint16_t crc_eeprom = eeprom_read_word((uint16_t*)CRC_LOCATION_EEPROM);
-    //uint16_t crc_flash = PGM_READ(CRC_LOCATION_FLASH);
-//
-    //if (crc_eeprom != crc_flash)   {
-//
-        //eeprom_update_word((uint16_t*)CRC_LOCATION_EEPROM, crc_flash);
-        //return true;
-//
-    //}   return false;
-//
-    //return false;
-//
-//}
+    //current app crc is written to last flash location
+    //previous crc is stored into eeprom
+    //if two differ, app has changed
+
+    uint16_t crc_eeprom = eeprom_read_word((uint16_t*)CRC_LOCATION_EEPROM);
+    uint16_t crc_flash = pgm_read_word_near(CRC_LOCATION_FLASH);
+
+    if (crc_eeprom != crc_flash)   {
+
+        eeprom_update_word((uint16_t*)CRC_LOCATION_EEPROM, crc_flash);
+        return true;
+
+    }   return false;
+
+    return false;
+
+}
 
 uint8_t getSWversion(swVersion_t point) {
 
@@ -36,8 +34,7 @@ uint8_t getSWversion(swVersion_t point) {
         case swVersion_minor:
         case swVersion_revision:
         case swVersion_development:
-        //return pgm_read_word(0);
-        return 5;
+        return (uint8_t)pgm_read_word_far(VERSION_POINT_LOCATION+(uint8_t)point*2);
 
         default:
         return 0;
