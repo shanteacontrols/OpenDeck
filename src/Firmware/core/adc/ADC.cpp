76bf5396aa21ee8176774adf3de14ba78c359092
paradajz <2544094+paradajz@users.noreply.github.com>
2016-09-18 15:33:33 +0200

    nicer adc config

--- src/Firmware/core/adc/ADC.cpp
+++ src/Firmware/core/adc/ADC.cpp
@@ -17,49 +17,42 @@
 */
 
 #include "ADC.h"
-#include <avr/cpufunc.h>
 #include "Config.h"
+#include <avr/cpufunc.h>
 
 void setUpADC() {
 
     ADMUX = 0x00;
     ADCSRA = 0x0;
 
-    //default ADC voltage is set to AREF
-
-    //set prescaler to 128 and enable ADC
-    ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
-
-}
-
-void setADCprescaler(uint8_t prescaler) {
-
-    //disable ADC before setting new prescaler
-    ADCSRA &= ~(1<<ADEN);
-
-    switch(prescaler)   {
-
-        case 16:
-        ADCSRA |= (1<<ADPS2);
-        break;
-
-        case 32:
-        ADCSRA |= (1<<ADPS2)|(1<<ADPS0);
-        break;
-
-        case 64:
-        ADCSRA |= (1<<ADPS2)|(1<<ADPS1);
-        break;
-
-        case 128:
+    //setup adc prescaler
+    #if ADC_PRESCALER == 128
         ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
-        break;
+    #elif ADC_PRESCALER == 64
+        ADCSRA |= (1<<ADPS2)|(1<<ADPS1);
+    #elif ADC_PRESCALER == 32
+        ADCSRA |= (1<<ADPS2)|(1<<ADPS0);
+    #elif ADC_PRESCALER == 16
+        ADCSRA |= (1<<ADPS2);
+    #else
+        #error Incorrect ADC prescaler setting. Valid values are 128, 64, 32 and 16.
+    #endif
 
-        default:
-        return;
-        break;
+    //setup voltage reference
+    #ifdef VREF_AREF
+        //nothing, this is default setting
+    #elif defined(VREF_AVCC)
+        ADMUX |= (1<<REFS0);
+    #elif defined(VREF_INTERNAL_2V56)
+        ADMUX |= (1<<REFS0) | (1<<REFS1);
+    #elif defined(VREF_INTERNAL_1V1)
+        ADMUX |= (1<<REFS1);
+    #else
+        #error "No ADC reference selected or setting is invalid. Valid options are VREF_AREF, VREF_AVCC, VREF_INTERNAL_2V56 and VREF_INTERNAL_1V1"
+    #endif
 
-    }   ADCSRA |= (1<<ADEN);
+    //enable ADC
+    ADCSRA |= (1<<ADEN);
 
 }
 
--- src/Firmware/core/adc/Config.h
+++ src/Firmware/core/adc/Config.h
@@ -20,5 +20,9 @@
 #define ADC_CONFIG_H_
 
 #define ADC_PRESCALER   128
+//#define VREF_AREF
+#define VREF_AVCC
+//#define VREF_INTERNAL_2V56
+//#define VREF_INTERNAL_1V1
 
 #endif
