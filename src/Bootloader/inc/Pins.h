f4196b4bdc334e9e0feda9cf0fe043fb8d63a0cc
paradajz <2544094+paradajz@users.noreply.github.com>
2017-01-30 20:59:44 +0100

    turn on both in and out leds while in bootloader

--- src/Bootloader/inc/Pins.h
+++ src/Bootloader/inc/Pins.h
@@ -1,8 +1,11 @@
 #pragma once
 
-#define LED_PORT    PORTE
-#define LED_PIN     6
+#define LED_IN_PORT     PORTE
+#define LED_IN_PIN      6
 
-#define RX_TX_PORT  PORTD
-#define TX_PIN      3
-#define RX_PIN      2
+#define LED_OUT_PORT    PORTE
+#define LED_OUT_PIN     2
+
+#define RX_TX_PORT      PORTD
+#define TX_PIN          3
+#define RX_PIN          2
