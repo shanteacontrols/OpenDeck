24ef3080e213c0e143056d8dc2f4c84143c1c78c
paradajz <ig.petrovi@outlook.com>
2017-02-08 19:52:53 +0100

    rename board dir to v1, set correct hardware version

--- src/Firmware/version/Hardware.h
+++ src/Firmware/version/Hardware.h
@@ -18,18 +18,10 @@
 
 #pragma once
 
+#ifdef BOARD_V1
 #define HARDWARE_VERSION_MAJOR      1
-#define HARDWARE_VERSION_MINOR      0
+#define HARDWARE_VERSION_MINOR      1
 #define HARDWARE_VERSION_REVISION   1
-
-const struct
-{
-    uint8_t major;
-    uint8_t minor;
-    uint8_t revision;
-} hardwareVersion =
-{
-    HARDWARE_VERSION_MAJOR,
-    HARDWARE_VERSION_MINOR,
-    HARDWARE_VERSION_REVISION
-};
+#else
+#error Wrong board selected
+#endif
