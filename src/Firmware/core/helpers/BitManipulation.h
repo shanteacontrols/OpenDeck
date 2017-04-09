a47e9011ca6bdf00f228eb4acb4ac99e737bcdc3
paradajz <ig.petrovi@outlook.com>
2017-04-09 11:20:27 +0200

    formatting

--- src/Firmware/core/helpers/BitManipulation.h
+++ src/Firmware/core/helpers/BitManipulation.h
@@ -21,9 +21,9 @@
 /// \defgroup coreBitHelpers Bit manipulation
 /// \ingroup coreHelpers
 /// @{
-#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
-#define bitSet(value, bit) ((value) |= (1UL << (bit)))
-#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
-#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
-#define invertByte(value) ((value) ^ 0xFF)
+#define bitRead(value, bit)             (((value) >> (bit)) & 0x01)
+#define bitSet(value, bit)              ((value) |= (1UL << (bit)))
+#define bitClear(value, bit)            ((value) &= ~(1UL << (bit)))
+#define bitWrite(value, bit, bitvalue)  (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+#define invertByte(value)               ((value) ^ 0xFF)
 /// @}
