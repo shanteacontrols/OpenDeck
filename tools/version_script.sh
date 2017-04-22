30aa315102d4964158475f0fead1345050dd0855
paradajz <ig.petrovi@outlook.com>
2017-04-23 00:03:23 +0200

    update/simplify tools

--- tools/version_script.sh
+++ tools/version_script.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 #read MAJOR file
-major=`cat MAJOR`
+major=`cat ../version/MAJOR`
 
 #read MINOR file
-minor=`cat MINOR`
+minor=`cat ../version/MINOR`
 
 #read REVISION
-revision=`cat REVISION`
+revision=`cat ../version/REVISION`
 
 #output $major, $minor and $revision into separate files
 echo "software version: $major.$minor.$revision"
 echo "$major,$minor,$revision" > version
+
+srec_cat FLASH.hex --intel --fill 0xFF 0x0000 0x67FE -b-e-crc16 0x67FE -XMODEM -o FLASH.bin -binary
+rm FLASH.hex
