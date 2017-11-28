bacdf86c446ce1b355966b6e375a06a8ec2a471d
paradajz <ig.petrovi@outlook.com>
2017-11-28 23:13:31 +0100

    switch build to Make, HID bootloader, arduino board work, misc

--- src/Arduino_16u2_Serial2USBMIDI/Release/lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Endpoint_RW.d
+++
@@ -1,3 +0,0 @@
-lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Endpoint_RW.d \
- lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Endpoint_RW.o: \
- ../lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Endpoint_RW.c
--- src/Arduino_16u2_Serial2USBMIDI/Release/lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Pipe_RW.d
+++
@@ -1,3 +0,0 @@
-lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Pipe_RW.d \
- lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Pipe_RW.o: \
- ../lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Pipe_RW.c
