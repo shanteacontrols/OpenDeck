543902ab020ff6729764751c1509a268fba6e892
paradajz <2544094+paradajz@users.noreply.github.com>
2017-12-04 22:44:35 +0100

    uncomment midi stuff

--- src/firmware/board/avr/variants/arduino16u2/main.cpp
+++ src/firmware/board/avr/variants/arduino16u2/main.cpp
@@ -7,9 +7,9 @@ int main(void)
 {
     board.init();
 
-    //midi.setUSBMIDIstate(true);
-    //midi.setDINMIDIstate(true);
-    //midi.setOneByteParseDINstate(true);
+    midi.setUSBMIDIstate(true);
+    midi.setDINMIDIstate(true);
+    midi.setOneByteParseDINstate(true);
 
     setLow(LED_OUT_PORT, LED_OUT_PIN);
     setLow(LED_IN_PORT, LED_IN_PIN);
@@ -22,16 +22,16 @@ int main(void)
     while (1)
     {
         //route data from uart to usb
-        //if (midi.read(dinInterface, THRU_FULL_USB))
-        //{
-            //MIDImessageSent = true;
-        //}
+        if (midi.read(dinInterface, THRU_FULL_USB))
+        {
+            MIDIsent = true;
+        }
 
         //route data from usb to uart
-        //if (midi.read(usbInterface, THRU_FULL_DIN))
-        //{
-            //MIDImessageReceived = true;
-        //}
+        if (midi.read(usbInterface, THRU_FULL_DIN))
+        {
+            MIDIreceived = true;
+        }
     }
 }
 
