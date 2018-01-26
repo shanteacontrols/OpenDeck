6ff450c05548cae51f6cdd158bc689ef1bf2daa8
paradajz <ig.petrovi@outlook.com>
2018-01-26 23:48:38 +0100

    add special sysex command to retrieve fw and hw info

--- src/firmware/OpenDeck.h
+++ src/firmware/OpenDeck.h
@@ -21,13 +21,14 @@
 #include "sysex/src/SysEx.h"
 #include "midi/src/MIDI.h"
 
-#define FIRMWARE_VERSION_STRING         0x56
-#define HARDWARE_VERSION_STRING         0x42
-#define REBOOT_APP_STRING               0x7F
-#define REBOOT_BTLDR_STRING             0x55
-#define FACTORY_RESET_STRING            0x44
-#define COMPONENT_ID_STRING             0x49
-#define MAX_COMPONENTS_STRING           0x4D
+#define FIRMWARE_VERSION_STRING             0x56
+#define HARDWARE_VERSION_STRING             0x42
+#define FIRMWARE_HARDWARE_VERSION_STRING    0x43
+#define REBOOT_APP_STRING                   0x7F
+#define REBOOT_BTLDR_STRING                 0x55
+#define FACTORY_RESET_STRING                0x44
+#define COMPONENT_ID_STRING                 0x49
+#define MAX_COMPONENTS_STRING               0x4D
 
 extern SysEx sysEx;
 extern MIDI midi;
