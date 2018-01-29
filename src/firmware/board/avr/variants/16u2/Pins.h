09296ee214dc259c1ccc1459dc2e35e7c0367ef7
paradajz <2544094+paradajz@users.noreply.github.com>
2018-01-29 12:18:58 +0100

    add pin definitions for btldr entry button

--- src/firmware/board/avr/variants/16u2/Pins.h
+++ src/firmware/board/avr/variants/16u2/Pins.h
@@ -18,8 +18,11 @@
 
 #pragma once
 
-#define LED_OUT_PORT    PORTD
-#define LED_OUT_PIN     5
+#define LED_OUT_PORT        PORTD
+#define LED_OUT_PIN         5
 
-#define LED_IN_PORT     PORTD
-#define LED_IN_PIN      4
+#define LED_IN_PORT         PORTD
+#define LED_IN_PIN          4
+
+#define BTLDR_BUTTON_PORT   PORTB
+#define BTLDR_BUTTON_PIN    2
