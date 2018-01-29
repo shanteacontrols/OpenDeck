d93fb1be6e835d70f795129fad73ad0c6dc4772a
paradajz <ig.petrovi@outlook.com>
2018-01-29 12:29:46 +0100

    checkNewRevision function tweaks

--- src/firmware/board/avr/variants/leonardo/Board.cpp
+++ src/firmware/board/avr/variants/leonardo/Board.cpp
@@ -18,18 +18,15 @@
 
 #include "Board.h"
 
-//flash code length -- this is loaded in flash by the linker and other scripts
-const uint32_t ProgramLength __attribute__ ((section (".length"))) = 0;
+///
+/// \brief Placeholder variable used only to reserve space in linker section.
+///
+const uint32_t appLength __attribute__ ((section (".applen"))) __attribute__((used)) = 0;
 
 ///
 /// \brief Location at which size of application is written in flash.
 ///
 #define APP_LENGTH_LOCATION         (uint32_t)0x000000AC
-
-///
-/// \brief Location at which compiled binary CRC is written in EEPROM.
-///
-#define SW_CRC_LOCATION_EEPROM     (EEPROM_SIZE - 2)
 
 Board::Board()
 {
@@ -58,13 +55,13 @@ void Board::reboot(rebootType_t type)
 ///
 bool Board::checkNewRevision()
 {
-    //current app crc is written to last flash location
+    //current app crc is written in ".applen" linker section
     //previous crc is stored into eeprom
     //if two differ, app has changed
 
-    uint32_t flash_size = pgm_read_dword_far(APP_LENGTH_LOCATION);
+    uint32_t flash_size = pgm_read_dword(APP_LENGTH_LOCATION);
     uint16_t crc_eeprom = eeprom_read_word((uint16_t*)SW_CRC_LOCATION_EEPROM);
-    uint16_t crc_flash = pgm_read_word_far(flash_size);
+    uint16_t crc_flash = pgm_read_word(flash_size);
 
     if (crc_eeprom != crc_flash)
     {
