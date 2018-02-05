b8f58b2fcb6ce85c3fdab2f9cee3440b01b2181e
paradajz <ig.petrovi@outlook.com>
2018-02-05 18:14:35 +0100

    includes in bootloader for teensy

--- src/bootloader/hid/BootloaderHID.h
+++ src/bootloader/hid/BootloaderHID.h
@@ -61,6 +61,9 @@
 #elif defined(BOARD_A_16u2)
 #include "../../firmware/board/avr/variants/16u2/Pins.h"
 #include "../../firmware/board/avr/variants/16u2/Hardware.h"
+#elif defined(BOARD_T_2PP)
+#include "../../firmware/board/avr/variants/teensy2pp/pins/Pins.h"
+#include "../../firmware/board/avr/variants/teensy2pp/Hardware.h"
 #endif
 
 /* Preprocessor Checks: */
