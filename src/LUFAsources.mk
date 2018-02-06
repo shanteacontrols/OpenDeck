80d0a9441fe826b0188f12dfc404c57362a03914
paradajz <ig.petrovi@outlook.com>
2018-02-06 20:52:30 +0100

    enable incremental build when only headers change, misc

--- src/LUFAsources.mk
+++ src/LUFAsources.mk
@@ -2,36 +2,36 @@
 ifeq ($(filter %mega %uno, $(MAKECMDGOALS)), )
     #common for bootloader and firmware
     LUFA_OBJS := \
-    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Device_AVR8.o \
-    modules/lufa/LUFA/Drivers/USB/Core/AVR8/EndpointStream_AVR8.o \
-    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Endpoint_AVR8.o \
-    modules/lufa/LUFA/Drivers/USB/Core/AVR8/PipeStream_AVR8.o \
-    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Pipe_AVR8.o \
-    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Endpoint_Control_R.o \
-    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Endpoint_Control_W.o \
-    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Endpoint_RW.o \
-    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Pipe_RW.o \
-    modules/lufa/LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.o \
-    modules/lufa/LUFA/Drivers/USB/Core/AVR8/USBInterrupt_AVR8.o \
-    modules/lufa/LUFA/Drivers/USB/Core/ConfigDescriptors.o \
-    modules/lufa/LUFA/Drivers/USB/Core/DeviceStandardReq.o \
-    modules/lufa/LUFA/Drivers/USB/Core/Events.o \
-    modules/lufa/LUFA/Drivers/USB/Core/USBTask.o
+    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Device_AVR8.c \
+    modules/lufa/LUFA/Drivers/USB/Core/AVR8/EndpointStream_AVR8.c \
+    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Endpoint_AVR8.c \
+    modules/lufa/LUFA/Drivers/USB/Core/AVR8/PipeStream_AVR8.c \
+    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Pipe_AVR8.c \
+    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Endpoint_Control_R.c \
+    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Endpoint_Control_W.c \
+    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Endpoint_RW.c \
+    modules/lufa/LUFA/Drivers/USB/Core/AVR8/Template/Template_Pipe_RW.c \
+    modules/lufa/LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.c \
+    modules/lufa/LUFA/Drivers/USB/Core/AVR8/USBInterrupt_AVR8.c \
+    modules/lufa/LUFA/Drivers/USB/Core/ConfigDescriptors.c \
+    modules/lufa/LUFA/Drivers/USB/Core/DeviceStandardReq.c \
+    modules/lufa/LUFA/Drivers/USB/Core/Events.c \
+    modules/lufa/LUFA/Drivers/USB/Core/USBTask.c
 
     #additional sources differ for firmware and bootloader
     ifeq ($(findstring boot,$(MAKECMDGOALS)), boot)
         #bootloader
         LUFA_OBJS += \
-        bootloader/hid/BootloaderHID.o \
-        bootloader/hid/Descriptors.o \
-        modules/lufa/LUFA/Drivers/USB/Class/Common/HIDParser.o \
-        modules/lufa/LUFA/Drivers/USB/Class/Device/HIDClassDevice.o
+        bootloader/hid/BootloaderHID.c \
+        bootloader/hid/Descriptors.c \
+        modules/lufa/LUFA/Drivers/USB/Class/Common/HIDParser.c \
+        modules/lufa/LUFA/Drivers/USB/Class/Device/HIDClassDevice.c
     else
         #firmware
         LUFA_OBJS += \
-        firmware/board/avr/usb/Descriptors.o \
-        modules/lufa/LUFA/Drivers/USB/Class/Device/AudioClassDevice.o \
-        modules/lufa/LUFA/Drivers/USB/Class/Device/MIDIClassDevice.o
+        firmware/board/avr/usb/Descriptors.c \
+        modules/lufa/LUFA/Drivers/USB/Class/Device/AudioClassDevice.c \
+        modules/lufa/LUFA/Drivers/USB/Class/Device/MIDIClassDevice.c
     endif
 else
     #no need for lufa
