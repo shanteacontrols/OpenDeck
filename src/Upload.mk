a0dce6c56016555c12522cc9b276cf47c1392e34
paradajz <ig.petrovi@outlook.com>
2018-02-14 21:21:09 +0100

    use arduino as isp for upload process instead of usbaso (more reliable)

--- src/Upload.mk
+++ src/Upload.mk
@@ -1,5 +1,5 @@
 #---------------- Programming Options (avrdude) ----------------
 
-AVR_DUDE_PATH := ../../../tools/avrdude/
-AVR_DUDE_BIN := avrdude
-AVRDUDE_PROGRAMMER = usbasp
+AVR_DUDE_BIN_PATH := ../../arduino/hardware/tools/avr/bin/avrdude
+AVR_DUDE_CONF_PATH := ../../arduino/hardware/tools/avr/etc/avrdude.conf
+AVRDUDE_PROGRAMMER = avrisp
