2bd36a2c873e0ad9af008994564b34fa1f1e449e
paradajz <ig.petrovi@outlook.com>
2018-02-19 12:12:50 +0100

    update licence headers

--- src/firmware/board/avr/variants/opendeck/Init.cpp
+++ src/firmware/board/avr/variants/opendeck/Init.cpp
@@ -1,6 +1,6 @@
 /*
     OpenDeck MIDI platform firmware
-    Copyright (C) 2015-2017 Igor Petrovic
+    Copyright (C) 2015-2018 Igor Petrovic
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
