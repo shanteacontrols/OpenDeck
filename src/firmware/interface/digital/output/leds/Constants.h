b9cd7501a7458fa4538e794559f26920751461c7
paradajz <2544094+paradajz@users.noreply.github.com>
2018-03-08 22:49:13 +0100

    remove unused defines

--- src/firmware/interface/digital/output/leds/Constants.h
+++ src/firmware/interface/digital/output/leds/Constants.h
@@ -20,10 +20,6 @@
 
 #include "../../../../database/Database.h"
 
-//define extra sysex sections for setting/getting led states
-#define ledColorSection                 LED_SECTIONS
-#define ledBlinkSection                 LED_SECTIONS+1
-
 #define BLINK_TIME_MIN                  0x02
 #define BLINK_TIME_MAX                  0x0F
 
