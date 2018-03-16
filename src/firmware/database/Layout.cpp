28e95e88bc47a5abe0df11dd0b8a7f664cab5c73
paradajz <2544094+paradajz@users.noreply.github.com>
2018-03-16 15:24:39 +0100

    modigy layout to match with updated dbms

--- src/firmware/database/Layout.cpp
+++ src/firmware/database/Layout.cpp
@@ -19,290 +19,362 @@
 #include "Database.h"
 #include "../board/Board.h"
 
+static dbSection_t midiSections[DB_SECTIONS_MIDI] =
+{
+    //midi feature section
+    {
+        .numberOfParameters = MIDI_FEATURES,
+        .parameterType = BIT_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //midi thru section
+    {
+        .numberOfParameters = MIDI_MERGE_OPTIONS,
+        .parameterType = HALFBYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    }
+};
+
+static dbSection_t buttonSections[DB_SECTIONS_BUTTONS] =
+{
+    //type section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_BUTTONS+MAX_NUMBER_OF_ANALOG,
+        .parameterType = BIT_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //midi message type section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_BUTTONS+MAX_NUMBER_OF_ANALOG,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //midi id section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_BUTTONS+MAX_NUMBER_OF_ANALOG,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = true,
+        .address = 0
+    },
+
+    //midi velocity section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_BUTTONS+MAX_NUMBER_OF_ANALOG,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 127,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //midi channel section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_BUTTONS+MAX_NUMBER_OF_ANALOG,
+        .parameterType = HALFBYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    }
+};
+
+static dbSection_t encoderSections[DB_SECTIONS_ENCODERS] =
+{
+    //encoder enabled section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ENCODERS,
+        .parameterType = BIT_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //encoder inverted section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ENCODERS,
+        .parameterType = BIT_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //encoding mode section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ENCODERS,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //midi id section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ENCODERS,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = true,
+        .address = 0
+    },
+
+    //midi channel section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ENCODERS,
+        .parameterType = HALFBYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    }
+};
+
+static dbSection_t analogSections[DB_SECTIONS_ANALOG] =
+{
+    //analog enabled section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ANALOG,
+        .parameterType = BIT_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //analog inverted section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ANALOG,
+        .parameterType = BIT_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //analog type section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ANALOG,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //midi id section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ANALOG,
+        .parameterType = WORD_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = true,
+        .address = 0
+    },
+
+    //lower cc limit
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ANALOG,
+        .parameterType = WORD_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //upper cc limit
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ANALOG,
+        .parameterType = WORD_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 16383,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //midi channel section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_ANALOG,
+        .parameterType = HALFBYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    }
+};
+
+static dbSection_t ledSections[DB_SECTIONS_LEDS] =
+{
+    //hardware parameters section
+    {
+        .numberOfParameters = LED_HARDWARE_PARAMETERS,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //activation id section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_LEDS,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = true,
+        .address = 0
+    },
+
+    //rgb enabled section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_RGB_LEDS,
+        .parameterType = BIT_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //local led control enabled section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_LEDS,
+        .parameterType = BIT_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //single velocity value section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_LEDS,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 127,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //midi channel section
+    {
+        .numberOfParameters = MAX_NUMBER_OF_LEDS,
+        .parameterType = HALFBYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    }
+};
+
+static dbSection_t displaySections[DB_SECTIONS_DISPLAY] =
+{
+    //features section
+    {
+        .numberOfParameters = DISPLAY_FEATURES,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    },
+
+    //hw section
+    {
+        .numberOfParameters = DISPLAY_HW_PARAMETERS,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = 0,
+        .autoIncrement = false,
+        .address = 0
+    }
+};
+
+static dbSection_t idSections[1] =
+{
+    {
+        .numberOfParameters = ID_BYTES,
+        .parameterType = BYTE_PARAMETER,
+        .preserveOnPartialReset = 0,
+        .defaultValue = UNIQUE_ID,
+        .autoIncrement = false,
+        .address = 0
+    },
+};
+
+dbBlock_t dbLayout[DB_BLOCKS] =
+{
+    //midi block
+    {
+        .address = 0,
+        .numberOfSections = DB_SECTIONS_MIDI,
+        .section = midiSections,
+    },
+
+    //buttons block
+    {
+        .address = 0,
+        .numberOfSections = DB_SECTIONS_BUTTONS,
+        .section = buttonSections,
+    },
+
+    //encoder block
+    {
+        .address = 0,
+        .numberOfSections = DB_SECTIONS_ENCODERS,
+        .section = encoderSections,
+    },
+
+    //analog block
+    {
+        .address = 0,
+        .numberOfSections = DB_SECTIONS_ANALOG,
+        .section = analogSections,
+    },
+
+    //led block
+    {
+        .address = 0,
+        .numberOfSections = DB_SECTIONS_LEDS,
+        .section = ledSections,
+    },
+
+    //display block
+    {
+        .address = 0,
+        .numberOfSections = DB_SECTIONS_DISPLAY,
+        .section = displaySections,
+    },
+
+    //id block
+    {
+        .address = 0,
+        .numberOfSections = 1,
+        .section = idSections,
+    },
+};
+
 ///
 /// \brief Creates database layout by defining blocks and sections.
 ///
 void Database::createLayout()
 {
-    DBMS::addBlocks(DB_BLOCKS);
-
-    dbSection_t section;
-
-    {
-        //MIDI block
-
-        //midi feature section
-        section.numberOfParameters = MIDI_FEATURES;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BIT_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_MIDI, section);
-
-        //midi thru section
-        section.numberOfParameters = MIDI_MERGE_OPTIONS;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = HALFBYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_MIDI, section);
-    }
-
-    {
-        //button block
-
-        //type section
-        section.numberOfParameters = MAX_NUMBER_OF_BUTTONS+MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BIT_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_BUTTONS, section);
-
-        //midi message type section
-        section.numberOfParameters = MAX_NUMBER_OF_BUTTONS+MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_BUTTONS, section);
-
-        //midi id section
-        section.numberOfParameters = MAX_NUMBER_OF_BUTTONS+MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 0;
-        section.autoIncrement = true;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_BUTTONS, section);
-
-        //midi velocity section
-        section.numberOfParameters = MAX_NUMBER_OF_BUTTONS+MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 127;
-        section.autoIncrement = false;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_BUTTONS, section);
-
-        //midi channel section
-        section.numberOfParameters = MAX_NUMBER_OF_BUTTONS+MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = HALFBYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_BUTTONS, section);
-    }
-
-    {
-        //encoder block
-
-        //encoder enabled section
-        section.numberOfParameters = MAX_NUMBER_OF_ENCODERS;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BIT_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ENCODERS, section);
-
-        //encoder inverted section
-        section.numberOfParameters = MAX_NUMBER_OF_ENCODERS;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BIT_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ENCODERS, section);
-
-        //encoding mode section
-        section.numberOfParameters = MAX_NUMBER_OF_ENCODERS;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ENCODERS, section);
-
-        //midi id section
-        section.numberOfParameters = MAX_NUMBER_OF_ENCODERS;
-        section.defaultValue = 0;
-        section.autoIncrement = true;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ENCODERS, section);
-
-        //midi channel section
-        section.numberOfParameters = MAX_NUMBER_OF_ENCODERS;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = HALFBYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ENCODERS, section);
-    }
-
-    {
-        //analog block
-
-        //analog enabled section
-        section.numberOfParameters = MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BIT_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ANALOG, section);
-
-        //analog inverted section
-        section.numberOfParameters = MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BIT_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ANALOG, section);
-
-        //analog type section
-        section.numberOfParameters = MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ANALOG, section);
-
-        //midi id section
-        section.numberOfParameters = MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 0;
-        section.autoIncrement = true;
-        section.parameterType = WORD_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ANALOG, section);
-
-        //lower cc limit
-        section.numberOfParameters = MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = WORD_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ANALOG, section);
-
-        //upper cc limit
-        section.numberOfParameters = MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 16383;
-        section.autoIncrement = false;
-        section.parameterType = WORD_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ANALOG, section);
-
-        //midi channel section
-        section.numberOfParameters = MAX_NUMBER_OF_ANALOG;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = HALFBYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ANALOG, section);
-    }
-
-    {
-        //led block
-
-        //hardware parameters section
-        section.numberOfParameters = LED_HARDWARE_PARAMETERS;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_LEDS, section);
-
-        //activation id section
-        section.numberOfParameters = MAX_NUMBER_OF_LEDS;
-        section.defaultValue = 0;
-        section.autoIncrement = true;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_LEDS, section);
-
-        //rgb enabled section
-        section.numberOfParameters = MAX_NUMBER_OF_RGB_LEDS;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BIT_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_LEDS, section);
-
-        //local led control enabled section
-        section.numberOfParameters = MAX_NUMBER_OF_LEDS;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BIT_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_LEDS, section);
-
-        //single velocity value section
-        section.numberOfParameters = MAX_NUMBER_OF_LEDS;
-        section.defaultValue = 127;
-        section.autoIncrement = false;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_LEDS, section);
-
-        //midi channel section
-        section.numberOfParameters = MAX_NUMBER_OF_LEDS;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = HALFBYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_LEDS, section);
-    }
-
-    {
-        //display block
-        //features section
-        section.numberOfParameters = DISPLAY_FEATURES;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_DISPLAY, section);
-
-        //hw section
-        section.numberOfParameters = DISPLAY_HW_PARAMETERS;
-        section.defaultValue = 0;
-        section.autoIncrement = false;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_DISPLAY, section);
-    }
-
-    {
-        //ID block
-
-        section.numberOfParameters = ID_BYTES;
-        section.defaultValue = UNIQUE_ID;
-        section.autoIncrement = false;
-        section.parameterType = BYTE_PARAMETER;
-        section.preserveOnPartialReset = 0;
-
-        DBMS::addSection(DB_BLOCK_ID, section);
-    }
+    database.commitLayout(dbLayout, DB_BLOCKS);
 }
