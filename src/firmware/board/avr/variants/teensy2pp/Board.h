71e33a87c64fb9a68985b5407ae1d5344863c30d
paradajz <2544094+paradajz@users.noreply.github.com>
2018-03-19 09:36:54 +0100

    add definition for led flashing function for teensy

--- src/firmware/board/avr/variants/teensy2pp/Board.h
+++ src/firmware/board/avr/variants/teensy2pp/Board.h
@@ -47,6 +47,7 @@ class Board
     Board();
     static void init();
     static bool checkNewRevision();
+    static void ledFlashStartup(bool fwUpdated);
 
     //digital in
     static bool digitalInputDataAvailable();
