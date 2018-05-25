39e1ea6eed398a48c3953d366009c905b19c0a19
paradajz <2544094+paradajz@users.noreply.github.com>
2018-05-25 16:18:44 +0200

    path again

--- webui/app/common/menu/menu.js
+++ webui/app/common/menu/menu.js
@@ -25,7 +25,7 @@
          return {
             restrict: 'E',
             replace: true,
-            templateUrl: 'OpenDeck/webui/app/common/menu/menu.html'
+            templateUrl: '/OpenDeck/webui/app/common/menu/menu.html'
 
     }});
 
