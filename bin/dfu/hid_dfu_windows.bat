36470ca69496fa67c0444538078e4a71d073d841
paradajz <2544094+paradajz@users.noreply.github.com>
2018-06-12 10:57:45 +0200

    add fw updater for linux, re-organize dfu dir

--- bin/dfu/hid_dfu_windows.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-loader\hid_bootloader_loader_win.exe atmega32u4 ..\compiled\board\opendeck\v1.2.x\fw_opendeck.hex
---
+++ bin/dfu/scripts/hid_dfu_linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../loader/hid_bootloader_loader_linux atmega32u4 ../../compiled/board/opendeck/v1.2.x/fw_opendeck.hex
