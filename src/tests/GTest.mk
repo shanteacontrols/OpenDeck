0f589883ff3e6e66da2173516ab9ecb7c9bb3f69
paradajz <2544094+paradajz@users.noreply.github.com>
2018-11-02 16:45:51 +0100

    remove redundant includes

--- src/tests/GTest.mk
+++ src/tests/GTest.mk
@@ -8,12 +8,12 @@ GTEST_SRCS_ := $(GTEST_DIR)/src/*.cc $(GTEST_DIR)/src/*.h $(GTEST_HEADERS)
 build/gtest-all.o: $(GTEST_SRCS_)
 	@mkdir -p $(@D)
 	@echo Building gtest-all.cc
-	@$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) $(CXXFLAGS) -c $(GTEST_DIR)/src/gtest-all.cc -o "$@"
+	@$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(GTEST_DIR)/src/gtest-all.cc -o "$@"
 
 build/gtest_main.o: $(GTEST_SRCS_)
 	@mkdir -p $(@D)
 	@echo Building gtest_main.cc
-	@$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) $(CXXFLAGS) -c $(GTEST_DIR)/src/gtest_main.cc -o "$@"
+	@$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(GTEST_DIR)/src/gtest_main.cc -o "$@"
 
 build/gtest_main.a: build/gtest-all.o build/gtest_main.o
 	@mkdir -p $(@D)
