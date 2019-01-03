4e4e0eafbf418373c7940db88b25a391ad0d982a
paradajz <2544094+paradajz@users.noreply.github.com>
2019-01-03 17:48:43 +0100

    fix max amount of analog components in tests

--- src/tests/interface/analog/Potentiometer.cpp
+++ src/tests/interface/analog/Potentiometer.cpp
@@ -10,7 +10,7 @@
 //this represents theoretical maximum amount of analog components
 //this test is run for all board variants
 //make sure that the default is largest amount of analog components
-#define MAX_MIDI_MESSAGES   32
+#define MAX_MIDI_MESSAGES   48
 
 uint8_t ccValue[MAX_MIDI_MESSAGES];
 uint8_t ccMessageCounter;
