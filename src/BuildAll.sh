0a1d3c4d210573867e1aa41b0b57f178a5a1d42e
paradajz <2544094+paradajz@users.noreply.github.com>
2019-04-11 23:35:18 +0200

    simplify BuildAll.sh script

--- src/BuildAll.sh
+++ src/BuildAll.sh
@@ -3,26 +3,10 @@
 #exit on error
 set -e
 
-#firmare/bootloader
-make clean && make fw_opendeck
-make clean && make fw_pro_micro
-make clean && make fw_leonardo
-make clean && make fw_uno
-make clean && make fw_mega
-make clean && make fw_mega6mux
-make clean && make fw_16u2
-make clean && make fw_8u2
-make clean && make fw_teensy2pp
-make clean && make fw_tannin
-make clean && make fw_kodama
-make clean && make fw_bergamot
-make clean && make boot_opendeck
-make clean && make boot_leonardo
-make clean && make boot_pro_micro
-make clean && make boot_teensy2pp
-make clean && make boot_kodama
-make clean && make boot_bergamot
-make clean && make boot_mega
-make clean && make boot_uno
-make clean && make boot_16u2 VARIANT=VARIANT_MEGA
-make clean && make boot_16u2 VARIANT=VARIANT_UNO
+#build all possible firmare/bootloader binaries
+targets=targets.txt
+
+while IFS= read -r line || [[ -n $line ]]
+do
+    make clean && make TARGETNAME=$line
+done < "$targets"
