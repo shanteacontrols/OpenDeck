bd29d49c1762a24ab99e3c23929f97cf3bf78720
paradajz <2544094+paradajz@users.noreply.github.com>
2019-05-09 14:17:08 +0200

    more detailed debounce tests for analog components

--- src/tests/interface/analog/Potentiometer.cpp
+++ src/tests/interface/analog/Potentiometer.cpp
@@ -133,13 +133,6 @@ TEST_F(PotentiometerTest, CCtest)
 
     EXPECT_EQ(MIDIstub::detail::ccMessageCounter, MAX_NUMBER_OF_ANALOG);
 
-    //try again with the same values
-    //no values should be sent
-    MIDIstub::reset();
-    analog.update();
-
-    EXPECT_EQ(MIDIstub::detail::ccMessageCounter, 0);
-
     //reset all values
     for (int i=0; i<MAX_NUMBER_OF_ANALOG; i++)
         analog.debounceReset(i);
@@ -153,34 +146,6 @@ TEST_F(PotentiometerTest, CCtest)
     for (int i=0; i<MAX_NUMBER_OF_ANALOG; i++)
         EXPECT_EQ(MIDIstub::detail::ccValue[i], MIDI_7_BIT_VALUE_MAX);
 
-    EXPECT_EQ(MIDIstub::detail::ccMessageCounter, MAX_NUMBER_OF_ANALOG);
-
-    //test the following scenario:
-    //1) record new value
-    //2) change direction
-    //3) verify that the new value cannot be recorded unless difference between them is ANALOG_STEP_MIN_DIFF_7_BIT*2
-
-    for (int i=0; i<MAX_NUMBER_OF_ANALOG; i++)
-        analog.debounceReset(i);
-
-    MIDIstub::reset();
-    Board::detail::adcReturnValue = 512;
-    expectedValue = mapRange(Board::detail::adcReturnValue, static_cast<uint32_t>(0), static_cast<uint32_t>(ADC_MAX_VALUE), static_cast<uint32_t>(0), static_cast<uint32_t>(MIDI_7_BIT_VALUE_MAX));
-    analog.update();
-    EXPECT_EQ(MIDIstub::detail::ccMessageCounter, MAX_NUMBER_OF_ANALOG);
-
-    //no values should be sent for now
-    MIDIstub::reset();
-    Board::detail::adcReturnValue = 512-ANALOG_STEP_MIN_DIFF_7_BIT;
-    expectedValue = mapRange(Board::detail::adcReturnValue, static_cast<uint32_t>(0), static_cast<uint32_t>(ADC_MAX_VALUE), static_cast<uint32_t>(0), static_cast<uint32_t>(MIDI_7_BIT_VALUE_MAX));
-    analog.update();
-    EXPECT_EQ(MIDIstub::detail::ccMessageCounter, 0);
-
-    //enough difference has been reached -> new values should be sent
-    MIDIstub::reset();
-    Board::detail::adcReturnValue = 512-(ANALOG_STEP_MIN_DIFF_7_BIT*2);
-    expectedValue = mapRange(Board::detail::adcReturnValue, static_cast<uint32_t>(0), static_cast<uint32_t>(ADC_MAX_VALUE), static_cast<uint32_t>(0), static_cast<uint32_t>(MIDI_7_BIT_VALUE_MAX));
-    analog.update();
     EXPECT_EQ(MIDIstub::detail::ccMessageCounter, MAX_NUMBER_OF_ANALOG);
 }
 
@@ -368,4 +333,127 @@ TEST_F(PotentiometerTest, ScalingAndInversion)
 
     for (int i=0; i<MAX_NUMBER_OF_ANALOG; i++)
         EXPECT_EQ(MIDIstub::detail::pbValue[i], expectedValue);
+}
+
+TEST_F(PotentiometerTest, DebouncingSetup)
+{
+    //verify that the step diff is properly configured
+    //don't allow step difference which would result in scenario where total number
+    //of generated values would be less than 127
+    EXPECT_TRUE((ADC_MAX_VALUE/ANALOG_STEP_MIN_DIFF_7_BIT) >= 127);
+}
+
+TEST_F(PotentiometerTest, Debouncing)
+{
+    //set known state
+    for (int i=0; i<MAX_NUMBER_OF_ANALOG; i++)
+    {
+        //enable all analog components
+        EXPECT_EQ(database.update(DB_BLOCK_ANALOG, dbSection_analog_enable, i, 1), true);
+
+        //disable invert state
+        EXPECT_EQ(database.update(DB_BLOCK_ANALOG, dbSection_analog_invert, i, 0), true);
+
+        //configure all analog components as potentiometers with CC MIDI message
+        EXPECT_EQ(database.update(DB_BLOCK_ANALOG, dbSection_analog_type, i, aType_potentiometer_cc), true);
+
+        //set all lower limits to 0
+        EXPECT_EQ(database.update(DB_BLOCK_ANALOG, dbSection_analog_lowerLimit, i, 0), true);
+
+        //set all upper limits to MIDI_14_BIT_VALUE_MAX
+        EXPECT_EQ(database.update(DB_BLOCK_ANALOG, dbSection_analog_upperLimit, i, MIDI_14_BIT_VALUE_MAX), true);
+    }
+
+    uint32_t expectedValue;
+    uint32_t newMIDIvalue;
+
+    MIDIstub::reset();
+    Board::detail::adcReturnValue = 0;
+    expectedValue = mapRange(Board::detail::adcReturnValue, static_cast<uint32_t>(ADC_MIN_VALUE), static_cast<uint32_t>(ADC_MAX_VALUE), static_cast<uint32_t>(0), static_cast<uint32_t>(MIDI_7_BIT_VALUE_MAX));
+    analog.update();
+
+    for (int i=0; i<MAX_NUMBER_OF_ANALOG; i++)
+        EXPECT_EQ(MIDIstub::detail::ccValue[i], expectedValue);
+
+    EXPECT_EQ(MIDIstub::detail::ccMessageCounter, MAX_NUMBER_OF_ANALOG);
+
+    //try again with the same values
+    //no values should be sent
+    MIDIstub::reset();
+    analog.update();
+
+    EXPECT_EQ(MIDIstub::detail::ccMessageCounter, 0);
+
+    //now test using different raw adc value which results in same MIDI value
+    MIDIstub::reset();
+
+    if ((ADC_MAX_VALUE/ANALOG_STEP_MIN_DIFF_7_BIT) > 127)
+    {
+        //it is possible that internally, ANALOG_STEP_MIN_DIFF_7_BIT is defined in a way
+        //which would still result in the same MIDI value being generated
+        //analog class shouldn't sent the same MIDI value even in this case
+        Board::detail::adcReturnValue += ANALOG_STEP_MIN_DIFF_7_BIT;
+        //verify that the newly generated value is indeed the same as the last one
+        EXPECT_EQ(expectedValue, mapRange(Board::detail::adcReturnValue, static_cast<uint32_t>(ADC_MIN_VALUE), static_cast<uint32_t>(ADC_MAX_VALUE), static_cast<uint32_t>(0), static_cast<uint32_t>(MIDI_7_BIT_VALUE_MAX)));
+        analog.update();
+        //no values should be sent since the resulting midi value is the same
+        EXPECT_EQ(MIDIstub::detail::ccMessageCounter, 0);
+
+        //now increase the raw value until the newly generated midi value differs from the last one
+        uint32_t newMIDIvalue;
+        do
+        {
+            Board::detail::adcReturnValue += 1;
+            newMIDIvalue = mapRange(Board::detail::adcReturnValue, static_cast<uint32_t>(ADC_MIN_VALUE), static_cast<uint32_t>(ADC_MAX_VALUE), static_cast<uint32_t>(0), static_cast<uint32_t>(MIDI_7_BIT_VALUE_MAX));
+        }
+        while (newMIDIvalue == expectedValue);
+
+        expectedValue = newMIDIvalue;
+
+        //verify that the values have been sent now
+        MIDIstub::reset();
+        analog.update();
+        EXPECT_EQ(MIDIstub::detail::ccMessageCounter, MAX_NUMBER_OF_ANALOG);
+
+        for (int i=0; i<MAX_NUMBER_OF_ANALOG; i++)
+            EXPECT_EQ(MIDIstub::detail::ccValue[i], expectedValue);
+    }
+    else
+    {
+        Board::detail::adcReturnValue += (ANALOG_STEP_MIN_DIFF_7_BIT-1);
+        //verify that the newly generated value is indeed the same as the last one
+        EXPECT_EQ(expectedValue, mapRange(Board::detail::adcReturnValue, static_cast<uint32_t>(ADC_MIN_VALUE), static_cast<uint32_t>(ADC_MAX_VALUE), static_cast<uint32_t>(0), static_cast<uint32_t>(MIDI_7_BIT_VALUE_MAX)));
+        analog.update();
+        //no values should be sent since the resulting midi value is the same
+        EXPECT_EQ(MIDIstub::detail::ccMessageCounter, 0);
+
+        Board::detail::adcReturnValue += 1;
+        //verify that the newly generated value differs from the last one
+        newMIDIvalue = mapRange(Board::detail::adcReturnValue, static_cast<uint32_t>(ADC_MIN_VALUE), static_cast<uint32_t>(ADC_MAX_VALUE), static_cast<uint32_t>(0), static_cast<uint32_t>(MIDI_7_BIT_VALUE_MAX));
+        EXPECT_FALSE(expectedValue == newMIDIvalue);
+        expectedValue = newMIDIvalue;
+        analog.update();
+        //values should be sent now
+        EXPECT_EQ(MIDIstub::detail::ccMessageCounter, MAX_NUMBER_OF_ANALOG);
+
+        for (int i=0; i<MAX_NUMBER_OF_ANALOG; i++)
+            EXPECT_EQ(MIDIstub::detail::ccValue[i], expectedValue);
+    }
+
+    //verify that the debouncing works in both ways
+    //change the direction
+
+    for (int i=0; i<ANALOG_STEP_MIN_DIFF_7_BIT-1; i++)
+    {
+        MIDIstub::reset();
+        Board::detail::adcReturnValue -= 1;
+        analog.update();
+        EXPECT_EQ(MIDIstub::detail::ccMessageCounter, 0);
+    }
+
+    //this time, values should be sent
+    MIDIstub::reset();
+    Board::detail::adcReturnValue -= 1;
+    analog.update();
+    EXPECT_EQ(MIDIstub::detail::ccMessageCounter, MAX_NUMBER_OF_ANALOG);
 }
