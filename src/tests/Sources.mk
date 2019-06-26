66cf95cdfe5ce470ba59416006bc2622334240a4
paradajz <2544094+paradajz@users.noreply.github.com>
2019-06-26 15:55:18 +0200

    add initial tests for buttons

--- src/tests/Sources.mk
+++ src/tests/Sources.mk
@@ -8,12 +8,39 @@ modules/dbms/src/LESSDB.cpp \
 stubs/database/DB_ReadWrite.cpp \
 application/database/Database.cpp
 
+SOURCES_buttons := \
+interface/digital/input/Buttons.cpp \
+stubs/Core.cpp \
+stubs/database/DB_ReadWrite.cpp \
+modules/dbms/src/LESSDB.cpp \
+application/interface/digital/input/buttons/Buttons.cpp \
+application/interface/digital/input/buttons/Hooks.cpp \
+application/interface/digital/output/leds/LEDs.cpp \
+application/interface/digital/input/Common.cpp \
+application/database/Database.cpp \
+modules/midi/src/MIDI.cpp \
+application/interface/display/U8X8/U8X8.cpp \
+application/interface/display/UpdateLogic.cpp \
+application/interface/display/TextBuild.cpp \
+application/interface/display/strings/Strings.cpp \
+modules/u8g2/csrc/u8x8_string.c \
+modules/u8g2/csrc/u8x8_setup.c \
+modules/u8g2/csrc/u8x8_u8toa.c \
+modules/u8g2/csrc/u8x8_8x8.c \
+modules/u8g2/csrc/u8x8_u16toa.c \
+modules/u8g2/csrc/u8x8_display.c \
+modules/u8g2/csrc/u8x8_fonts.c \
+modules/u8g2/csrc/u8x8_byte.c \
+modules/u8g2/csrc/u8x8_cad.c \
+modules/u8g2/csrc/u8x8_gpio.c \
+modules/u8g2/csrc/u8x8_d_ssd1306_128x64_noname.c \
+modules/u8g2/csrc/u8x8_d_ssd1306_128x32.c
+
 SOURCES_encoders := \
 stubs/Core.cpp \
 stubs/database/DB_ReadWrite.cpp \
 modules/dbms/src/LESSDB.cpp \
 application/interface/digital/input/encoders/Encoders.cpp \
-application/interface/digital/output/leds/LEDs.cpp \
 application/interface/digital/input/Common.cpp \
 application/database/Database.cpp \
 interface/digital/input/Encoders.cpp \
