608df3a8a5dadb237558d1cbd342b82369163543
paradajz <2544094+paradajz@users.noreply.github.com>
2019-11-19 22:26:51 +0100

    make generated Objects.mk file more readable

--- scripts/gen_test_targets.sh
+++ scripts/gen_test_targets.sh
@@ -2,7 +2,7 @@
 
 tests=$(find ./src -maxdepth 1 -name "test_*" -type d | cut -d/ -f3)
 
-echo TESTS=$tests > Objects.mk
+echo TESTS := $tests > Objects.mk
 printf '%s\n' 'TESTS_EXT := $(addprefix $(BUILD_DIR)/,$(TESTS))' >> Objects.mk
 printf '%s\n\n' 'TESTS_EXT := $(addsuffix .out,$(TESTS_EXT))' >> Objects.mk
 printf '%s\n' '.DEFAULT_GOAL := all' >> Objects.mk
@@ -28,5 +28,5 @@ do
     printf '%s\n' '$(BUILD_DIR)/'${test}'.out: $(OBJECTS_'${test}') $(TEST_FRAMEWORK_OBJECTS)' >> Objects.mk
     printf '\t%s\n' '@echo Linking' >> Objects.mk
     printf '\t%s\n' '@$(CXX) $(LDFLAGS) $(COMMON_FLAGS) $(CPP_FLAGS) $^ -o $@' >> Objects.mk
-    printf '\t%s\n' '@echo Created executable: $@' >> Objects.mk
+    printf '\t%s\n\n' '@echo Created executable: $@' >> Objects.mk
 done
