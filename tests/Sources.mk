202fc1e61b92fa5bd5b765ca907368cd95736b40
paradajz <2544094+paradajz@users.noreply.github.com>
2019-11-19 15:28:20 +0100

    move tests and .vscode dirs to root dir

--- tests/Sources.mk
+++ tests/Sources.mk
@@ -1,7 +1,7 @@
-vpath application/%.cpp ../
-vpath common/%.cpp ../
-vpath modules/%.cpp ../
-vpath modules/%.c ../
+vpath application/%.cpp ../src
+vpath common/%.cpp ../src
+vpath modules/%.cpp ../src
+vpath modules/%.c ../src
 
 TEST_FRAMEWORK_SOURCES := \
 modules/unity/src/unity.c
@@ -98,12 +98,12 @@ common/OpenDeckMIDIformat/OpenDeckMIDIformat.cpp
 #common include dirs
 INCLUDE_DIRS := \
 -I"./" \
--I"../application/" \
--I"../" \
--I"../modules/" \
--I"../board/avr/variants/$(MCU)/$(subst fw_,,$(TARGETNAME))/" \
+-I"../src/application/" \
+-I"../src/" \
+-I"../src/modules/" \
+-I"../src/board/avr/variants/$(MCU)/$(subst fw_,,$(TARGETNAME))/" \
 -isystem "stubs/avr"
 
 INCLUDE_FILES += \
--include "../board/avr/variants/$(MCU)/$(subst fw_,,$(TARGETNAME))/Hardware.h" \
--include "../board/avr/Config.h"
+-include "../src/board/avr/variants/$(MCU)/$(subst fw_,,$(TARGETNAME))/Hardware.h" \
+-include "../src/board/avr/Config.h"
