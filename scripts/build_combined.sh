1fcc5805c5bd7314543c1b171e1bb27bbfb819c7
paradajz <2544094+paradajz@users.noreply.github.com>
2020-01-04 14:17:19 +0100

    rework script used to build combined fw+btldr

---
+++ scripts/build_combined.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function usage
+{
+    echo -e "Used to create hex file containing both the bootloader and application\n"
+    echo -e "Usage: ./`basename "$0"` target\n"
+    echo -e "target    \tBoard and firmware type to compile. See targets.txt for list of supported targets. Prefix in target is ignored (fw_ and boot_)"
+}
+
+if [[ ("$*" == "--help") || ($# -eq 0) ]]
+then
+    usage
+    exit 1
+fi
+
+TARGET=$1
+
+boot_target=${TARGET/fw_/boot_}
+fw_target=${TARGET/boot_/fw_}
+board=${fw_target/fw_/}
+
+#bootloader target might not exist - verify
+boot_dir=$(make TARGETNAME=$boot_target print-BUILD_DIR)
+
+if [[ ($? -ne 0) ]]
+then
+    exit 1
+fi
+
+fw_dir=$(make TARGETNAME=$fw_target print-BUILD_DIR)
+
+#always build release fw type when running this script
+combined_dir=$(make print-BUILD_DIR_BASE)/fw_boot_$board/release
+
+#build bootloader
+echo Building $boot_target...
+make TARGETNAME=$boot_target
+
+#build firmware
+echo Building $fw_target...
+make TARGETNAME=$fw_target
+
+echo Merging bootloader and firmware...
+mkdir -p $combined_dir
+srec_cat $fw_dir/$fw_target.hex -intel $boot_dir/$boot_target.hex -Intel -o $combined_dir/fw_boot_$board.hex -Intel
+echo Firmware merged: $combined_dir/fw_boot_$board.hex
