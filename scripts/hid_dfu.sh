a4db5a4bfa9bbf08dc878388b8086e97fd706a88
paradajz <2544094+paradajz@users.noreply.github.com>
2020-05-03 19:58:18 +0200

    scripts: simplify hid_dfu.sh script - don't require GNU find

--- scripts/hid_dfu.sh
+++ scripts/hid_dfu.sh
@@ -9,31 +9,22 @@ then
 fi
 
 if [ "$(uname)" == "Darwin" ]; then
-    find="gfind"
-
-    if [[ "$(command -v gfind)" == "" ]]
-    then
-        echo "ERROR: GNU find not installed (gfind)"
-        exit 1
-    fi
-
     dfu="bin/dfu/hid_bootloader_loader_mac"
 elif [ "$(uname -s)" == "Linux" ]
 then
-    find="find"
     dfu="bin/dfu/hid_bootloader_loader_linux"
 fi
 
 echo "Info: In case of errors please run this script with super-user rights."
 echo "Please select the board for which you want to update firmware:"
 
-boards=$($find bin/compiled -type f -not -path "*merged*" -name "*.hex" -printf '%f\n' | cut -f 1 -d '.' | sort)
+boards=$(find bin/compiled/fw -type f -name "*.hex" -exec basename {} .hex \; | sort)
 echo "$boards" | tr " " "\n" | cat -n
 printf "Board number: "
 read -r board_nr
 
 filename=$(echo "$boards" | head -n "$board_nr" | tail -n 1)
-path=$($find -type f -not -path "*merged*" -name "${filename}.hex")
-mcu=$(echo "$path" | cut -d / -f6)
+path=$(find bin/compiled/fw -type f -name "${filename}.hex")
+mcu=$(echo "$path" | cut -d / -f5)
 
 "$dfu" "$mcu" "$path"
