6bd916b4eb7017d26e55c0c8ecdd8b7f810c0479
paradajz <2544094+paradajz@users.noreply.github.com>
2020-08-08 11:12:15 +0200

    touchscreen: don't hardcode number of components

--- src/Defines.mk
+++ src/Defines.mk
@@ -210,7 +210,7 @@ endif
 
 ifeq ($(shell yq r ../targets/$(TARGETNAME).yml touchscreen.use), true)
     DEFINES += TOUCHSCREEN_SUPPORTED
-    DEFINES += MAX_TOUCHSCREEN_BUTTONS=64
+    DEFINES += MAX_TOUCHSCREEN_BUTTONS=$(shell yq r ../targets/$(TARGETNAME).yml touchscreen.components)
     DEFINES += LEDS_SUPPORTED
 
     UART_CHANNEL_TOUCHSCREEN := $(shell yq r ../targets/$(TARGETNAME).yml touchscreen.uartChannel)
