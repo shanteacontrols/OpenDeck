058d3ffbc024e7a4a824cc9b30b3686d1c130afb
paradajz <2544094+paradajz@users.noreply.github.com>
2020-11-25 17:55:48 +0100

    bump version

--- src/application/Version.h
+++ src/application/Version.h
@@ -23,7 +23,7 @@ limitations under the License.
 /// @{
 
 #define SW_VERSION_MAJOR    5
-#define SW_VERSION_MINOR    0
-#define SW_VERSION_REVISION 10
+#define SW_VERSION_MINOR    1
+#define SW_VERSION_REVISION 0
 
 /// @}
