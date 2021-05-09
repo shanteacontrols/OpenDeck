88081ffb1b6609c69efa9d6ae6bb16b6576e5fd6
paradajz <2544094+paradajz@users.noreply.github.com>
2021-05-09 16:00:01 +0200

    tests: add forced component refresh tests, refactor test midi helpers

--- tests/helpers/MIDI.h
+++ tests/helpers/MIDI.h
@@ -16,99 +16,6 @@ class MIDIHelper
     public:
     MIDIHelper() = default;
 
-    static void sysExToUSBMIDIPacket(std::vector<uint8_t>& sysEx, std::vector<MIDI::USBMIDIpacket_t>& usbPacket)
-    {
-        usbPacket.clear();
-
-        size_t inLength   = sysEx.size();
-        size_t startIndex = 0;
-
-        while (inLength > 3)
-        {
-            usbPacket.push_back({ MIDI::USBMIDIEvent(0, static_cast<uint8_t>(MIDI::usbMIDIsystemCin_t::sysExStartCin)), sysEx[startIndex + 0], sysEx[startIndex + 1], sysEx[startIndex + 2] });
-            startIndex += 3;
-            inLength -= 3;
-        }
-
-        if (inLength == 3)
-        {
-            usbPacket.push_back({ MIDI::USBMIDIEvent(0, static_cast<uint8_t>(MIDI::usbMIDIsystemCin_t::sysExStop3byteCin)), sysEx[startIndex + 0], sysEx[startIndex + 1], sysEx[startIndex + 2] });
-        }
-        else if (inLength == 2)
-        {
-            usbPacket.push_back({ MIDI::USBMIDIEvent(0, static_cast<uint8_t>(MIDI::usbMIDIsystemCin_t::sysExStop2byteCin)), sysEx[startIndex + 0], sysEx[startIndex + 1], 0 });
-        }
-        else if (inLength == 1)
-        {
-            usbPacket.push_back({ MIDI::USBMIDIEvent(0, static_cast<uint8_t>(MIDI::usbMIDIsystemCin_t::sysExStop1byteCin)), sysEx[startIndex + 0], 0, 0 });
-        }
-    }
-
-    static bool parseUSBSysEx(std::vector<MIDI::USBMIDIpacket_t>& usbPacket, std::vector<uint8_t>& sysEx)
-    {
-        if (!usbPacket.size())
-            return false;
-
-        sysEx.clear();
-
-        while (usbPacket.size())
-        {
-            uint8_t midiMessage = usbPacket.at(0).Event << 4;
-
-            switch (midiMessage)
-            {
-            //1 byte messages
-            case static_cast<uint8_t>(MIDI::usbMIDIsystemCin_t::sysCommon1byteCin):
-            {
-                //end of sysex
-                sysEx.push_back(usbPacket.at(0).Data1);
-                return true;
-            }
-            break;
-
-            //sysex
-            case static_cast<uint8_t>(MIDI::usbMIDIsystemCin_t::sysExStartCin):
-            {
-                //the message can be any length between 3 and MIDI_SYSEX_ARRAY_SIZE
-                if (usbPacket.at(0).Data1 == 0xF0)
-                    sysEx.clear();
-
-                sysEx.push_back(usbPacket.at(0).Data1);
-                sysEx.push_back(usbPacket.at(0).Data2);
-                sysEx.push_back(usbPacket.at(0).Data3);
-            }
-            break;
-
-            case static_cast<uint8_t>(MIDI::usbMIDIsystemCin_t::sysExStop2byteCin):
-            {
-                sysEx.push_back(usbPacket.at(0).Data1);
-                sysEx.push_back(usbPacket.at(0).Data2);
-                return true;
-            }
-            break;
-
-            case static_cast<uint8_t>(MIDI::usbMIDIsystemCin_t::sysExStop3byteCin):
-            {
-                if (usbPacket.at(0).Data1 == 0xF0)
-                    sysEx.clear();    //sysex message with 1 byte of payload
-
-                sysEx.push_back(usbPacket.at(0).Data1);
-                sysEx.push_back(usbPacket.at(0).Data2);
-                sysEx.push_back(usbPacket.at(0).Data3);
-                return true;
-            }
-            break;
-
-            default:
-                break;
-            }
-
-            usbPacket.erase(usbPacket.begin());
-        }
-
-        return false;
-    }
-
     template<typename T>
     static bool setSingleSysExReq(T section, size_t index, uint16_t value)
     {
@@ -154,16 +61,16 @@ class MIDIHelper
             SYSEX_MANUFACTURER_ID_0,
             SYSEX_MANUFACTURER_ID_1,
             SYSEX_MANUFACTURER_ID_2,
-            static_cast<uint8_t>(SysExConf::status_t::request),
-            0,
-            static_cast<uint8_t>(SysExConf::wish_t::get),
-            static_cast<uint8_t>(SysExConf::amount_t::single),
-            static_cast<uint8_t>(blockIndex),
-            static_cast<uint8_t>(section),
-            splitIndex.high(),
-            splitIndex.low(),
-            0,
-            0,
+            static_cast<uint8_t>(SysExConf::status_t::request),    //status
+            0,                                                     //part
+            static_cast<uint8_t>(SysExConf::wish_t::get),          //wish
+            static_cast<uint8_t>(SysExConf::amount_t::single),     //amount
+            static_cast<uint8_t>(blockIndex),                      //block
+            static_cast<uint8_t>(section),                         //section
+            splitIndex.high(),                                     //index high byte
+            splitIndex.low(),                                      //index low byte
+            0x00,                                                  //new value high byte
+            0x00,                                                  //new value low byte
             0xF7
         };
     }
--- tests/src/system/test.cpp
+++ tests/src/system/test.cpp
@@ -1,3 +1,5 @@
+#ifndef USB_LINK_MCU
+
 #include "unity/Framework.h"
 #include "io/buttons/Buttons.h"
 #include "io/encoders/Encoders.h"
@@ -15,6 +17,16 @@
 
 namespace
 {
+    typedef struct
+    {
+        uint8_t             channel;    ///< MIDI channel on which the message was received (1-16)
+        MIDI::messageType_t type;       ///< The type of the message
+        uint8_t             data1;      ///< First data byte (0-127)
+        uint8_t             data2;      ///< Second data byte (0-127, 0 if message length is 2 bytes)
+    } channelMIDImessage_t;
+
+    std::vector<channelMIDImessage_t> channelMIDImessages;
+
     class SystemHWA : public System::HWA
     {
         public:
@@ -62,41 +74,56 @@ namespace
 
         bool init() override
         {
+            clear();
             return true;
         }
 
         bool dinRead(uint8_t& data) override
         {
-            return false;
+            if (!dinPacketToBoard.size())
+                return false;
+
+            data = dinPacketToBoard.at(0);
+            dinPacketToBoard.erase(dinPacketToBoard.begin());
+
+            return true;
         }
 
         bool dinWrite(uint8_t data) override
         {
-            dinPacketOut.push_back(data);
+            dinPacketFromBoard.push_back(data);
             return true;
         }
 
         bool usbRead(MIDI::USBMIDIpacket_t& USBMIDIpacket) override
         {
-            if (!usbPacketIn.size())
+            if (!usbPacketFromBoard.size())
                 return false;
 
-            USBMIDIpacket = usbPacketIn.at(0);
-            usbPacketIn.erase(usbPacketIn.begin());
+            USBMIDIpacket = usbPacketFromBoard.at(0);
+            usbPacketFromBoard.erase(usbPacketFromBoard.begin());
 
             return true;
         }
 
         bool usbWrite(MIDI::USBMIDIpacket_t& USBMIDIpacket) override
         {
-            usbPacketOut.push_back(USBMIDIpacket);
+            usbPacketToBoard.push_back(USBMIDIpacket);
             return true;
         }
 
-        std::vector<MIDI::USBMIDIpacket_t> usbPacketIn  = {};
-        std::vector<MIDI::USBMIDIpacket_t> usbPacketOut = {};
-        std::vector<uint8_t>               dinPacketIn  = {};
-        std::vector<uint8_t>               dinPacketOut = {};
+        void clear()
+        {
+            usbPacketFromBoard.clear();
+            usbPacketToBoard.clear();
+            dinPacketToBoard.clear();
+            dinPacketFromBoard.clear();
+        }
+
+        std::vector<MIDI::USBMIDIpacket_t> usbPacketFromBoard = {};
+        std::vector<MIDI::USBMIDIpacket_t> usbPacketToBoard   = {};
+        std::vector<uint8_t>               dinPacketToBoard   = {};
+        std::vector<uint8_t>               dinPacketFromBoard = {};
     } hwaMIDI;
 
     class HWALEDs : public IO::LEDs::HWA
@@ -255,14 +282,173 @@ namespace
     IO::Encoders    encoders(hwaEncoders, encodersFilter, 1, database, midi, display, cInfo);
     IO::Touchscreen touchscreen(database, cInfo);
     System          systemStub(hwaSystem, cInfo, database, midi, buttons, encoders, analog, leds, display, touchscreen);
+
+    void sendSysExRequest(const std::vector<uint8_t> request, std::vector<MIDI::USBMIDIpacket_t>& buffer)
+    {
+        class HWAFillMIDI : public MIDI::HWA
+        {
+            public:
+            HWAFillMIDI(std::vector<MIDI::USBMIDIpacket_t>& buffer)
+                : _buffer(buffer)
+            {}
+
+            bool init() override
+            {
+                return true;
+            }
+
+            bool dinRead(uint8_t& data) override
+            {
+                return false;
+            }
+
+            bool dinWrite(uint8_t data) override
+            {
+                return false;
+            }
+
+            bool usbRead(MIDI::USBMIDIpacket_t& USBMIDIpacket) override
+            {
+                return false;
+            }
+
+            bool usbWrite(MIDI::USBMIDIpacket_t& USBMIDIpacket) override
+            {
+                _buffer.push_back(USBMIDIpacket);
+                return true;
+            }
+
+            std::vector<MIDI::USBMIDIpacket_t>& _buffer;
+        } hwaFillMIDI(buffer);
+
+        //create temp midi object which will fill internal hwaMIDI buffer of the real midi object when send is called
+        //calling the real midi read will then result in parsing those filled bytes by the temp object
+        MIDI fillMIDI(hwaFillMIDI);
+
+        fillMIDI.enableUSBMIDI();
+        fillMIDI.sendSysEx(request.size(), &request[0], true);
+
+        //store this in a variable since every midi.read call will decrement the size of buffer
+        size_t packetSize = hwaFillMIDI._buffer.size();
+
+        //now just call system which will call midi.read which in turn will read the filled packets
+        for (size_t i = 0; i < packetSize; i++)
+            systemStub.run();
+    };
+
+    void verifyResponse(const std::vector<uint8_t> response, std::vector<MIDI::USBMIDIpacket_t>& buffer)
+    {
+        class HWAParseMIDI : public MIDI::HWA
+        {
+            public:
+            HWAParseMIDI(std::vector<MIDI::USBMIDIpacket_t>& buffer)
+                : _buffer(buffer)
+            {}
+
+            bool init() override
+            {
+                return true;
+            }
+
+            bool dinRead(uint8_t& data) override
+            {
+                return false;
+            }
+
+            bool dinWrite(uint8_t data) override
+            {
+                _parsed.push_back(data);
+                return true;
+            }
+
+            bool usbRead(MIDI::USBMIDIpacket_t& USBMIDIpacket) override
+            {
+                if (!_buffer.size())
+                    return false;
+
+                USBMIDIpacket = _buffer.at(0);
+                _buffer.erase(_buffer.begin());
+
+                return true;
+            }
+
+            bool usbWrite(MIDI::USBMIDIpacket_t& USBMIDIpacket) override
+            {
+                return true;
+            }
+
+            std::vector<MIDI::USBMIDIpacket_t>& _buffer;
+            std::vector<uint8_t>                _parsed;
+
+        } hwaParseMIDI(buffer);
+
+        MIDI parseMIDI(hwaParseMIDI);
+
+        parseMIDI.enableUSBMIDI();
+        parseMIDI.enableDINMIDI();
+        parseMIDI.setInputChannel(MIDI_CHANNEL_OMNI);
+
+        //create temp midi object which will read written usb packets and pass them back as DIN MIDI array
+        //for easier parsing
+
+        size_t packetSize              = hwaParseMIDI._buffer.size();
+        size_t sysExResponseIndexStart = 0;
+
+        for (size_t i = 0; i < packetSize; i++)
+        {
+            if (parseMIDI.read(MIDI::interface_t::usb, MIDI::filterMode_t::fullDIN))
+            {
+                auto    messageType = parseMIDI.getType(MIDI::interface_t::usb);
+                uint8_t data1       = parseMIDI.getData1(MIDI::interface_t::usb);
+                uint8_t data2       = parseMIDI.getData2(MIDI::interface_t::usb);
+                uint8_t channel     = parseMIDI.getChannel(MIDI::interface_t::usb);
+
+                bool chMessage = false;
+
+                switch (messageType)
+                {
+                case MIDI::messageType_t::noteOn:
+                case MIDI::messageType_t::noteOff:
+                case MIDI::messageType_t::controlChange:
+                case MIDI::messageType_t::programChange:
+                    chMessage = true;
+                    break;
+
+                default:
+                    break;
+                }
+
+                if (chMessage)
+                {
+                    channelMIDImessages.push_back({
+                        channel,
+                        messageType,
+                        data1,
+                        data2,
+                    });
+                }
+            }
+        }
+
+        for (sysExResponseIndexStart = 0; sysExResponseIndexStart < hwaParseMIDI._parsed.size(); sysExResponseIndexStart++)
+        {
+            if (hwaParseMIDI._parsed.at(sysExResponseIndexStart) != response.at(0))
+                continue;
+
+            for (size_t i = 0; i < hwaParseMIDI._parsed.size() - sysExResponseIndexStart; i++)
+                TEST_ASSERT_EQUAL_UINT32(response.at(i), hwaParseMIDI._parsed.at(i + sysExResponseIndexStart));
+
+            break;
+        }
+    };
 }    // namespace
 
-#ifndef USB_LINK_MCU
 TEST_SETUP()
 {
     hwaSystem.reset();
     midi.init();
     midi.enableUSBMIDI();
+    midi.disableDINMIDI();
 }
 
 TEST_CASE(SystemInit)
@@ -277,9 +463,9 @@ TEST_CASE(SystemInit)
     //now enable din midi via write in database
     TEST_ASSERT(database.update(Database::Section::global_t::midiFeatures, static_cast<size_t>(System::midiFeature_t::dinEnabled), 1) == true);
 
+    //init system again and verify that din midi is enabled
     TEST_ASSERT(systemStub.init() == true);
 
-//init system again and verify that din midi is enabled
 #ifdef DIN_MIDI_SUPPORTED
     TEST_ASSERT(hwaSystem.dinMIDIenabled == true);
     TEST_ASSERT(hwaSystem.loopbackEnabled == false);
@@ -312,118 +498,81 @@ TEST_CASE(Requests)
     //send fake usb midi message containing the handshake
     //verify that the response is valid
 
-    std::vector<uint8_t> request;
-    std::vector<uint8_t> response;
+    hwaMIDI.usbPacketToBoard.clear();
 
-    auto sendRequest = [&]() {
-        MIDIHelper::sysExToUSBMIDIPacket(request, hwaMIDI.usbPacketIn);
+    //handshake
+    sendSysExRequest({ 0xF0,
+                       0x00,
+                       0x53,
+                       0x43,
+                       0x00,
+                       0x00,
+                       0x01,
+                       0xF7 },
+                     hwaMIDI.usbPacketFromBoard);
 
-        size_t sz = hwaMIDI.usbPacketIn.size();
+    verifyResponse({ 0xF0,
+                     0x00,
+                     0x53,
+                     0x43,
+                     0x01,
+                     0x00,
+                     0x01,
+                     0xF7 },
+                   hwaMIDI.usbPacketToBoard);
 
-        for (size_t i = 0; i < sz; i++)
-            systemStub.run();
-    };
+    hwaMIDI.usbPacketFromBoard.clear();
 
-    auto verifyResponse = [&]() {
-        std::vector<uint8_t> parsed;
-        TEST_ASSERT(MIDIHelper::parseUSBSysEx(hwaMIDI.usbPacketOut, parsed) == true);
-        TEST_ASSERT(parsed == response);
-        hwaMIDI.usbPacketIn.clear();
-        hwaMIDI.usbPacketOut.clear();
-    };
-
-    request = {
-        0xF0,
-        0x00,
-        0x53,
-        0x43,
-        0x00,
-        0x00,
-        0x01,
-        0xF7
-    };
-
-    response = {
-        0xF0,
-        0x00,
-        0x53,
-        0x43,
-        0x01,
-        0x00,
-        0x01,
-        0xF7
-    };
-
-    sendRequest();
-    verifyResponse();
-
-    //try to enable DIN midi via sysex
-    //verify that it is disabled first
+    //verify that din midi is disabled
     TEST_ASSERT(hwaSystem.dinMIDIenabled == false);
-    hwaMIDI.dinPacketOut.clear();
     TEST_ASSERT(database.read(Database::Section::global_t::midiFeatures, static_cast<size_t>(System::midiFeature_t::dinEnabled)) == false);
-    midi.sendNoteOn(127, 127, 1);
-    TEST_ASSERT_EQUAL_UINT32(0, hwaMIDI.dinPacketOut.size());
-    hwaMIDI.usbPacketOut.clear();
 
-    request = {
-        0xF0,
-        0x00,
-        0x53,
-        0x43,
-        0x00,
-        0x00,
-        0x01,    //set
-        0x00,    //single
-        0x00,    //global block
-        0x00,    //midi features
-        0x00,
-        0x03,    //din midi
-        0x00,
-        0x01,    //enable
-        0xF7
-    };
+    midi.sendNoteOn(127, 127, 1);
+    TEST_ASSERT_EQUAL_UINT32(0, hwaMIDI.dinPacketFromBoard.size());
+
+    std::vector<uint8_t> generatedSysExReq;
+    MIDIHelper::generateSysExSetReq(System::Section::global_t::midiFeatures, static_cast<size_t>(System::midiFeature_t::dinEnabled), 1, generatedSysExReq);
+
+    hwaMIDI.clear();
+
+    sendSysExRequest(generatedSysExReq, hwaMIDI.usbPacketFromBoard);
 
 #ifdef DIN_MIDI_SUPPORTED
-    response = {
-        0xF0,
-        0x00,
-        0x53,
-        0x43,
-        0x01,
-        0x00,
-        0x01,    //set
-        0x00,    //single
-        0x00,    //global block
-        0x00,    //midi features
-        0x00,
-        0x03,    //din midi
-        0x00,
-        0x01,    //enable
-        0xF7
-    };
-#else
-    response = {
-        0xF0,
-        0x00,
-        0x53,
-        0x43,
-        0x0D,    //not supported error
-        0x00,
-        0x01,    //set
-        0x00,    //single
-        0x00,    //global block
-        0x00,    //midi features
-        0x00,
-        0x03,    //din midi
-        0x00,
-        0x01,    //enable
-        0xF7
-    };
-#endif
+    verifyResponse({ 0xF0,
+                     0x00,
+                     0x53,
+                     0x43,
+                     0x01,
+                     0x00,
+                     0x01,    //set
+                     0x00,    //single
+                     0x00,    //global block
+                     0x00,    //midi features
+                     0x00,
+                     0x03,    //din midi
+                     0x00,
+                     0x01,    //enable
+                     0xF7 },
+                   hwaMIDI.usbPacketToBoard);
 
-    sendRequest();
-    verifyResponse();
+#else
+    verifyResponse({ 0xF0,
+                     0x00,
+                     0x53,
+                     0x43,
+                     0x0D,    //not supported error
+                     0x00,
+                     0x01,    //set
+                     0x00,    //single
+                     0x00,    //global block
+                     0x00,    //midi features
+                     0x00,
+                     0x03,    //din midi
+                     0x00,
+                     0x01,    //enable
+                     0xF7 },
+                   hwaMIDI.usbPacketToBoard);
+#endif
 
 #ifdef DIN_MIDI_SUPPORTED
     TEST_ASSERT(hwaSystem.dinMIDIenabled == true);
@@ -438,42 +587,114 @@ TEST_CASE(Requests)
     midi.sendNoteOn(127, 127, 1);
 
 #ifdef DIN_MIDI_SUPPORTED
-    TEST_ASSERT_EQUAL_UINT32(3, hwaMIDI.dinPacketOut.size());
+    TEST_ASSERT_EQUAL_UINT32(3, hwaMIDI.dinPacketFromBoard.size());
 #else
-    TEST_ASSERT_EQUAL_UINT32(0, hwaMIDI.dinPacketOut.size());
+    TEST_ASSERT_EQUAL_UINT32(0, hwaMIDI.dinPacketFromBoard.size());
 #endif
 
-    hwaMIDI.usbPacketOut.clear();
-    hwaMIDI.usbPacketIn.clear();
-    hwaMIDI.dinPacketOut.clear();
-    hwaMIDI.dinPacketIn.clear();
+    hwaMIDI.usbPacketToBoard.clear();
+    hwaMIDI.usbPacketFromBoard.clear();
+    hwaMIDI.dinPacketFromBoard.clear();
+    hwaMIDI.dinPacketToBoard.clear();
 
     //send handshake again and verify that the response isn't sent to din midi
-    request = {
-        0xF0,
-        0x00,
-        0x53,
-        0x43,
-        0x00,
-        0x00,
-        0x01,
-        0xF7
-    };
+    sendSysExRequest({ 0xF0,
+                       0x00,
+                       0x53,
+                       0x43,
+                       0x00,
+                       0x00,
+                       0x01,
+                       0xF7 },
+                     hwaMIDI.usbPacketFromBoard);
 
-    response = {
-        0xF0,
-        0x00,
-        0x53,
-        0x43,
-        0x01,
-        0x00,
-        0x01,
-        0xF7
-    };
+    verifyResponse({ 0xF0,
+                     0x00,
+                     0x53,
+                     0x43,
+                     0x01,
+                     0x00,
+                     0x01,
+                     0xF7 },
+                   hwaMIDI.usbPacketToBoard);
 
-    sendRequest();
-    verifyResponse();
+    TEST_ASSERT_EQUAL_UINT32(0, hwaMIDI.dinPacketFromBoard.size());
+}
 
-    TEST_ASSERT_EQUAL_UINT32(0, hwaMIDI.dinPacketOut.size());
+TEST_CASE(ForcedResendOnPresetChange)
+{
+    database.factoryReset();
+    TEST_ASSERT(systemStub.init() == true);
+
+    //enable first analog component in first two presets
+    TEST_ASSERT(database.setPreset(1) == true);
+    TEST_ASSERT(database.update(Database::Section::analog_t::enable, 0, 1) == true);
+
+    TEST_ASSERT(database.setPreset(0) == true);
+    TEST_ASSERT(database.update(Database::Section::analog_t::enable, 0, 1) == true);
+
+    hwaMIDI.clear();
+
+    hwaAnalog.adcReturnValue = 127;
+    analog.update();
+
+    TEST_ASSERT_EQUAL_UINT32(1, hwaMIDI.usbPacketToBoard.size());
+
+    TEST_ASSERT_EQUAL_UINT32(MIDI::messageType_t::controlChange, hwaMIDI.usbPacketToBoard.at(0).Event << 4);
+    TEST_ASSERT_EQUAL_UINT32(MIDI::messageType_t::controlChange, hwaMIDI.usbPacketToBoard.at(0).Data1);
+    TEST_ASSERT_EQUAL_UINT32(0, hwaMIDI.usbPacketToBoard.at(0).Data2);
+    TEST_ASSERT_EQUAL_UINT32(127, hwaMIDI.usbPacketToBoard.at(0).Data3);
+
+    //now change preset and verify that the same midi message is repeated
+    hwaMIDI.clear();
+
+    //handshake
+    sendSysExRequest({ 0xF0,
+                       0x00,
+                       0x53,
+                       0x43,
+                       0x00,
+                       0x00,
+                       0x01,
+                       0xF7 },
+                     hwaMIDI.usbPacketFromBoard);
+
+    verifyResponse({ 0xF0,
+                     0x00,
+                     0x53,
+                     0x43,
+                     0x01,
+                     0x00,
+                     0x01,
+                     0xF7 },
+                   hwaMIDI.usbPacketToBoard);
+
+    hwaMIDI.clear();
+    channelMIDImessages.clear();
+
+    std::vector<uint8_t> generatedSysExReq;
+    MIDIHelper::generateSysExSetReq(System::Section::global_t::presets, static_cast<size_t>(System::presetSetting_t::activePreset), 1, generatedSysExReq);
+
+    sendSysExRequest(generatedSysExReq, hwaMIDI.usbPacketFromBoard);
+
+    verifyResponse({ 0xF0,
+                     0x00,
+                     0x53,
+                     0x43,
+                     0x01,
+                     0x00,
+                     0x01,    //set
+                     0x00,    //single
+                     0x00,    //block 0 (global)
+                     0x02,    //section 2 (presets)
+                     0x00,    //active preset
+                     0x00,
+                     0x00,    //preset 1
+                     0x01,
+                     0xF7 },
+                   hwaMIDI.usbPacketToBoard);
+
+    //since the preset has been changed, all buttons should resend their state and all enabled analog components (only 1 in this case)
+    TEST_ASSERT(channelMIDImessages.size() == MAX_NUMBER_OF_BUTTONS + 1);
 }
 #endif
