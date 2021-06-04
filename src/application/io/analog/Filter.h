fd6b239fc1234144e63678bd196c0984ebaa0977
paradajz <2544094+paradajz@users.noreply.github.com>
2021-06-04 14:40:38 +0200

    analog: slightly rework filter, naming improvements

--- src/application/io/analog/Filter.h
+++ src/application/io/analog/Filter.h
@@ -43,15 +43,6 @@ namespace IO
 
         bool isFiltered(size_t index, Analog::type_t type, uint16_t value, uint16_t& filteredValue) override
         {
-            auto compare = [](const void* a, const void* b) {
-                if (*(uint16_t*)a < *(uint16_t*)b)
-                    return -1;
-                else if (*(uint16_t*)a > *(uint16_t*)b)
-                    return 1;
-
-                return 0;
-            };
-
             value = CONSTRAIN(value, 0, _adcConfig.adcMaxValue);
 
             //avoid filtering in this case for faster response
@@ -67,23 +58,42 @@ namespace IO
                 return true;
             }
 
-            bool fastFilter = true;
+            auto compare = [](const void* a, const void* b) {
+                if (*(uint16_t*)a < *(uint16_t*)b)
+                    return -1;
+                else if (*(uint16_t*)a > *(uint16_t*)b)
+                    return 1;
+
+                return 0;
+            };
+
+            const bool     fastFilter = (core::timing::currentRunTimeMs() - _lastStableMovementTime[index]) < FAST_FILTER_ENABLE_AFTER_MS;
+            const bool     use14bit   = (type == Analog::type_t::nrpn14b) || (type == Analog::type_t::pitchBend) || (type == Analog::type_t::cc14bit);
+            const uint16_t maxLimit   = use14bit ? MIDI_14_BIT_VALUE_MAX : MIDI_7_BIT_VALUE_MAX;
+            const bool     direction  = value >= _lastStableValue[index];
+            const uint16_t stepDiff   = !use14bit || (direction != _lastStableDirection[index]) || !fastFilter ? _stepDiff7Bit : _adcConfig.stepDiff14Bit;
+
+            if (abs(value - _lastStableValue[index]) < stepDiff)
+            {
+                _medianSampleCounter[index]   = 0;
+                _repetitionSampleCount[index] = 0;
+                return false;
+            }
 
             if (index < MAX_NUMBER_OF_ANALOG)
             {
                 //don't filter the readings for touchscreen data
 
-                if ((core::timing::currentRunTimeMs() - _lastMovementTime[index]) > FAST_FILTER_ENABLE_AFTER_MS)
+                if (!fastFilter)
                 {
-                    fastFilter                                    = false;
-                    _analogSample[index][_sampleCounter[index]++] = value;
+                    _analogSample[index][_medianSampleCounter[index]++] = value;
 
                     //take the median value to avoid using outliers
-                    if (_sampleCounter[index] == 3)
+                    if (_medianSampleCounter[index] == 3)
                     {
                         qsort(_analogSample[index], 3, sizeof(uint16_t), compare);
-                        _sampleCounter[index] = 0;
-                        filteredValue         = _analogSample[index][1];
+                        _medianSampleCounter[index] = 0;
+                        filteredValue               = _analogSample[index][1];
                     }
                     else
                     {
@@ -100,53 +110,21 @@ namespace IO
                 filteredValue = value;
             }
 
-            bool     use14bit = false;
-            uint16_t maxLimit;
-            uint16_t stepDiff;
-
-            if ((type == Analog::type_t::nrpn14b) || (type == Analog::type_t::pitchBend) || (type == Analog::type_t::cc14bit))
-                use14bit = true;
-
-            if (use14bit)
-            {
-                maxLimit = MIDI_14_BIT_VALUE_MAX;
-                stepDiff = _adcConfig.stepDiff14Bit;
-            }
-            else
-            {
-                maxLimit = MIDI_7_BIT_VALUE_MAX;
-                stepDiff = _stepDiff7Bit;
-            }
-
-            //if the first read value is 0, mark it as increasing
-            bool direction = filteredValue >= _lastStableValue[index];
-
-            //always use 7bit diff value here regardless of the type of value (7bit/14bit)
-            //for 14bit value this will improve stability since those values are scaled anyways and use small diff value
-            if (direction != _lastDirection[index])
-                stepDiff = _stepDiff7Bit;
-
-            if (abs(filteredValue - _lastStableValue[index]) < stepDiff)
-            {
-                _stableSampleCount[index] = 0;
-                return false;
-            }
-
-            auto midiValue    = core::misc::mapRange(static_cast<uint32_t>(filteredValue), static_cast<uint32_t>(0), static_cast<uint32_t>(_adcConfig.adcMaxValue), static_cast<uint32_t>(0), static_cast<uint32_t>(maxLimit));
-            auto oldMIDIvalue = core::misc::mapRange(static_cast<uint32_t>(_lastStableValue[index]), static_cast<uint32_t>(0), static_cast<uint32_t>(_adcConfig.adcMaxValue), static_cast<uint32_t>(0), static_cast<uint32_t>(maxLimit));
+            const auto midiValue    = core::misc::mapRange(static_cast<uint32_t>(filteredValue), static_cast<uint32_t>(0), static_cast<uint32_t>(_adcConfig.adcMaxValue), static_cast<uint32_t>(0), static_cast<uint32_t>(maxLimit));
+            const auto oldMIDIvalue = core::misc::mapRange(static_cast<uint32_t>(_lastStableValue[index]), static_cast<uint32_t>(0), static_cast<uint32_t>(_adcConfig.adcMaxValue), static_cast<uint32_t>(0), static_cast<uint32_t>(maxLimit));
 
             if (midiValue == oldMIDIvalue)
             {
-                _stableSampleCount[index] = 0;
+                _repetitionSampleCount[index] = 0;
                 return false;
             }
 
             auto acceptNewValue = [&]() {
-                _stableSampleCount[index] = 0;
-                _lastDirection[index]     = direction;
-                _lastStableValue[index]   = filteredValue;
-                _sampleCounter[index]     = 0;
-                _lastMovementTime[index]  = core::timing::currentRunTimeMs();
+                _repetitionSampleCount[index]  = 0;
+                _lastStableDirection[index]    = direction;
+                _lastStableValue[index]        = filteredValue;
+                _medianSampleCounter[index]    = 0;
+                _lastStableMovementTime[index] = core::timing::currentRunTimeMs();
             };
 
             if (fastFilter)
@@ -155,7 +133,7 @@ namespace IO
             }
             else
             {
-                if (++_stableSampleCount[index] >= _stableValueRepetitions)
+                if (++_repetitionSampleCount[index] >= _stableValueRepetitions)
                     acceptNewValue();
                 else
                     return false;
@@ -172,14 +150,14 @@ namespace IO
 
             //when edge values are reached, disable fast filter by resetting last movement time
             if ((midiValue == 0) || (midiValue == maxLimit))
-                _lastMovementTime[index] = 0;
+                _lastStableMovementTime[index] = 0;
 
             return true;
         }
 
         void reset(size_t index) override
         {
-            _sampleCounter[index] = 0;
+            _medianSampleCounter[index] = 0;
         }
 
         private:
@@ -219,12 +197,12 @@ namespace IO
         const size_t                _stableValueRepetitions;
         const uint16_t              _stepDiff7Bit;
 
-        static constexpr uint32_t FAST_FILTER_ENABLE_AFTER_MS                                                     = 500;
-        uint16_t                  _analogSample[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS][3]   = {};
-        size_t                    _sampleCounter[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS]     = {};
-        bool                      _lastDirection[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS]     = {};
-        uint16_t                  _lastStableValue[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS]   = {};
-        uint8_t                   _stableSampleCount[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS] = {};
-        uint32_t                  _lastMovementTime[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS]  = {};
+        static constexpr uint32_t FAST_FILTER_ENABLE_AFTER_MS                                                          = 500;
+        uint16_t                  _analogSample[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS][3]        = {};
+        size_t                    _medianSampleCounter[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS]    = {};
+        bool                      _lastStableDirection[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS]    = {};
+        uint16_t                  _lastStableValue[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS]        = {};
+        uint8_t                   _repetitionSampleCount[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS]  = {};
+        uint32_t                  _lastStableMovementTime[MAX_NUMBER_OF_ANALOG + MAX_NUMBER_OF_TOUCHSCREEN_COMPONENTS] = {};
     };    // namespace IO
 }    // namespace IO
