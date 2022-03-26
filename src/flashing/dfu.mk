71ca6f456f899e8cf8529be54243ba0a1272a2c3
paradajz <2544094+paradajz@users.noreply.github.com>
2022-03-26 18:51:45 +0100

    build/flashing: fix broken dfu flash tool

--- src/flashing/dfu.mk
+++ src/flashing/dfu.mk
@@ -3,7 +3,8 @@
 flash:
 ifeq ($(shell uname), Linux)
 	@if ! [ $$(id -u) = 0 ]; then \
-	echo "dfu-util must be run as root user on Linux"; \
-	false;
+	echo "dfu-util must be run as root user or sudo on Linux"; \
+	false; \
+	fi
 endif
-	@sudo dfu-util -d 0483:df11 -a 0 -i 0 -s 0x8000000:leave -D $(FLASH_BINARY_DIR)/$(TARGET).bin
+	@dfu-util -d 0483:df11 -a 0 -i 0 -s 0x8000000:leave -D $(FLASH_BINARY_DIR)/$(TARGET).bin
