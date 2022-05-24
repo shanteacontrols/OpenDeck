335693d8f294d2b71a08cf16b74406e4b8763d68
paradajz <2544094+paradajz@users.noreply.github.com>
2022-05-24 18:49:52 +0200

    board/nrf52: switch to uarte peripheral

--- src/board/arch/arm/nordic/common/communication/uart/UART.cpp
+++ src/board/arch/arm/nordic/common/communication/uart/UART.cpp
@@ -22,88 +22,116 @@ limitations under the License.
 #include "board/Board.h"
 #include "board/Internal.h"
 #include "nrfx_prs.h"
-#include "nrfx_uart.h"
+#include "nrfx_uarte.h"
 #include <Target.h>
 
 namespace
 {
-    std::map<uint32_t, nrf_uart_baudrate_t> _baudRateMap = {
+    std::map<uint32_t, nrf_uarte_baudrate_t> _baudRateMap = {
         {
             1200,
-            NRF_UART_BAUDRATE_1200,
+            NRF_UARTE_BAUDRATE_1200,
         },
         {
             2400,
-            NRF_UART_BAUDRATE_2400,
+            NRF_UARTE_BAUDRATE_2400,
         },
         {
             4800,
-            NRF_UART_BAUDRATE_4800,
+            NRF_UARTE_BAUDRATE_4800,
         },
         {
             9600,
-            NRF_UART_BAUDRATE_9600,
+            NRF_UARTE_BAUDRATE_9600,
         },
         {
             14400,
-            NRF_UART_BAUDRATE_14400,
+            NRF_UARTE_BAUDRATE_14400,
         },
         {
             19200,
-            NRF_UART_BAUDRATE_19200,
+            NRF_UARTE_BAUDRATE_19200,
         },
         {
             28800,
-            NRF_UART_BAUDRATE_28800,
+            NRF_UARTE_BAUDRATE_28800,
         },
         {
             31250,
-            NRF_UART_BAUDRATE_31250,
+            NRF_UARTE_BAUDRATE_31250,
         },
         {
             38400,
-            NRF_UART_BAUDRATE_38400,
+            NRF_UARTE_BAUDRATE_38400,
         },
         {
             56000,
-            NRF_UART_BAUDRATE_56000,
+            NRF_UARTE_BAUDRATE_56000,
         },
         {
             57600,
-            NRF_UART_BAUDRATE_57600,
+            NRF_UARTE_BAUDRATE_57600,
         },
         {
             76800,
-            NRF_UART_BAUDRATE_76800,
+            NRF_UARTE_BAUDRATE_76800,
         },
         {
             115200,
-            NRF_UART_BAUDRATE_115200,
+            NRF_UARTE_BAUDRATE_115200,
         },
         {
             230400,
-            NRF_UART_BAUDRATE_230400,
+            NRF_UARTE_BAUDRATE_230400,
         },
         {
             250000,
-            NRF_UART_BAUDRATE_250000,
+            NRF_UARTE_BAUDRATE_250000,
         },
         {
             460800,
-            NRF_UART_BAUDRATE_460800,
+            NRF_UARTE_BAUDRATE_460800,
         },
         {
             921600,
-            NRF_UART_BAUDRATE_921600,
+            NRF_UARTE_BAUDRATE_921600,
         },
         {
             1000000,
-            NRF_UART_BAUDRATE_1000000,
+            NRF_UARTE_BAUDRATE_1000000,
         },
     };
 
-    nrfx_uart_t   _uartInstance[core::mcu::peripherals::MAX_UART_INTERFACES];
+    uint8_t       _nrfTxBuffer[core::mcu::peripherals::MAX_UART_INTERFACES];
+    uint8_t       _nrfRxBuffer[core::mcu::peripherals::MAX_UART_INTERFACES];
+    nrfx_uarte_t  _uartInstance[core::mcu::peripherals::MAX_UART_INTERFACES];
     volatile bool _transmitting[core::mcu::peripherals::MAX_UART_INTERFACES];
+
+    inline void checkTx(uint8_t channel)
+    {
+        size_t  remainingBytes;
+        uint8_t value;
+
+        if (Board::detail::UART::getNextByteToSend(channel, value, remainingBytes))
+        {
+            _nrfTxBuffer[channel] = value;
+            nrf_uarte_event_clear(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_ENDTX);
+            nrf_uarte_event_clear(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_TXSTOPPED);
+            nrf_uarte_tx_buffer_set(_uartInstance[channel].p_reg, &_nrfTxBuffer[channel], 1);
+            _transmitting[channel] = true;
+            nrf_uarte_task_trigger(_uartInstance[channel].p_reg, NRF_UARTE_TASK_STARTTX);
+        }
+        else
+        {
+            // Transmitter has to be stopped by triggering STOPTX task to achieve
+            // the lowest possible level of the UARTE power consumption.
+
+            _transmitting[channel] = false;
+
+            nrf_uarte_event_clear(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_TXSTOPPED);
+            nrf_uarte_task_trigger(_uartInstance[channel].p_reg, NRF_UARTE_TASK_STOPTX);
+        }
+    }
 }    // namespace
 
 namespace Board::detail::UART::MCU
@@ -113,25 +141,12 @@ namespace Board::detail::UART::MCU
         // On NRF52, there is no TX Empty interrupt - only TX ready
         // interrupt, which occurs after the data has been sent.
         // In order to trigger the interrupt initially, STARTTX task must be triggered
-        // and data needs to be written to TXD register.
+        // and data needs to be written to internal NRF buffer.
         // Do this only if current data transfer has completed.
 
         if (!_transmitting[channel])
         {
-            size_t  remainingBytes;
-            uint8_t value;
-
-            if (Board::detail::UART::getNextByteToSend(channel, value, remainingBytes))
-            {
-                _transmitting[channel] = true;
-                nrf_uart_event_clear(_uartInstance[channel].p_reg, NRF_UART_EVENT_TXDRDY);
-                nrf_uart_task_trigger(_uartInstance[channel].p_reg, NRF_UART_TASK_STARTTX);
-                nrf_uart_txd_set(_uartInstance[channel].p_reg, value);
-            }
-            else
-            {
-                nrf_uart_task_trigger(_uartInstance[channel].p_reg, NRF_UART_TASK_STOPTX);
-            }
+            checkTx(channel);
         }
     }
 
@@ -142,8 +157,16 @@ namespace Board::detail::UART::MCU
 
     bool deInit(uint8_t channel)
     {
-        nrf_uart_int_disable(_uartInstance[channel].p_reg, NRF_UART_INT_MASK_RXDRDY | NRF_UART_INT_MASK_TXDRDY | NRF_UART_INT_MASK_ERROR | NRF_UART_INT_MASK_RXTO);
+        nrf_uarte_int_disable(_uartInstance[channel].p_reg,
+                              NRF_UARTE_INT_ENDRX_MASK |
+                                  NRF_UARTE_INT_ENDTX_MASK |
+                                  NRF_UARTE_INT_ERROR_MASK |
+                                  NRF_UARTE_INT_RXTO_MASK |
+                                  NRF_UARTE_INT_TXSTOPPED_MASK);
+
         NRFX_IRQ_DISABLE(nrfx_get_irq_number((void*)_uartInstance[channel].p_reg));
+
+        nrf_uarte_disable(_uartInstance[channel].p_reg);
 
         CORE_MCU_IO_DEINIT(Board::detail::map::uartPins(channel).tx);
         CORE_MCU_IO_DEINIT(Board::detail::map::uartPins(channel).rx);
@@ -159,7 +182,13 @@ namespace Board::detail::UART::MCU
         {
         case 0:
         {
-            _uartInstance[channel] = NRFX_UART_INSTANCE(0);
+            _uartInstance[channel] = NRFX_UARTE_INSTANCE(0);
+        }
+        break;
+
+        case 1:
+        {
+            _uartInstance[channel] = NRFX_UARTE_INSTANCE(1);
         }
         break;
 
@@ -167,6 +196,7 @@ namespace Board::detail::UART::MCU
             return false;
         }
 
+        // unsupported by NRF52
         if (config.parity == Board::UART::parity_t::ODD)
         {
             return false;
@@ -184,39 +214,42 @@ namespace Board::detail::UART::MCU
 
         CORE_MCU_IO_SET_HIGH(Board::detail::map::uartPins(channel).tx.port, Board::detail::map::uartPins(channel).tx.index);
 
-        CORE_MCU_IO_INIT({ Board::detail::map::uartPins(channel).tx.port,
-                           Board::detail::map::uartPins(channel).tx.index,
-                           core::mcu::io::pinMode_t::OUTPUT_PP,
-                           core::mcu::io::pullMode_t::NONE });
+        CORE_MCU_IO_INIT(Board::detail::map::uartPins(channel).tx.port,
+                         Board::detail::map::uartPins(channel).tx.index,
+                         core::mcu::io::pinMode_t::OUTPUT_PP,
+                         core::mcu::io::pullMode_t::NONE);
 
-        CORE_MCU_IO_INIT({ Board::detail::map::uartPins(channel).rx.port,
-                           Board::detail::map::uartPins(channel).rx.index,
-                           core::mcu::io::pinMode_t::INPUT,
-                           core::mcu::io::pullMode_t::NONE });
+        CORE_MCU_IO_INIT(Board::detail::map::uartPins(channel).rx.port,
+                         Board::detail::map::uartPins(channel).rx.index,
+                         core::mcu::io::pinMode_t::INPUT,
+                         core::mcu::io::pullMode_t::NONE);
 
-        nrf_uart_config_t nrfUartConfig = {};
-        nrfUartConfig.hwfc              = NRF_UART_HWFC_DISABLED;
-        nrfUartConfig.parity            = config.parity ? NRF_UART_PARITY_INCLUDED : NRF_UART_PARITY_EXCLUDED;
+        nrfx_uarte_config_t nrfUartConfig = {};
+        nrfUartConfig.hal_cfg             = {
+                        .hwfc   = NRF_UARTE_HWFC_DISABLED,
+                        .parity = config.parity ? NRF_UARTE_PARITY_INCLUDED : NRF_UARTE_PARITY_EXCLUDED
+        };
 
-        nrf_uart_baudrate_set(_uartInstance[channel].p_reg, _baudRateMap[config.baudRate]);
-        nrf_uart_configure(_uartInstance[channel].p_reg, &nrfUartConfig);
-        nrf_uart_txrx_pins_set(_uartInstance[channel].p_reg,
-                               NRF_GPIO_PIN_MAP(Board::detail::map::uartPins(channel).tx.port,
-                                                Board::detail::map::uartPins(channel).tx.index),
-                               NRF_GPIO_PIN_MAP(Board::detail::map::uartPins(channel).rx.port,
-                                                Board::detail::map::uartPins(channel).rx.index));
+        nrf_uarte_baudrate_set(_uartInstance[channel].p_reg, _baudRateMap[config.baudRate]);
+        nrf_uarte_configure(_uartInstance[channel].p_reg, &nrfUartConfig.hal_cfg);
+        nrf_uarte_txrx_pins_set(_uartInstance[channel].p_reg,
+                                CORE_NRF_GPIO_PIN_MAP(Board::detail::map::uartPins(channel).tx.port,
+                                                      Board::detail::map::uartPins(channel).tx.index),
+                                CORE_NRF_GPIO_PIN_MAP(Board::detail::map::uartPins(channel).rx.port,
+                                                      Board::detail::map::uartPins(channel).rx.index));
 
-        nrf_uart_event_clear(_uartInstance[channel].p_reg, NRF_UART_EVENT_TXDRDY);
-        nrf_uart_event_clear(_uartInstance[channel].p_reg, NRF_UART_EVENT_RXTO);
-        nrf_uart_event_clear(_uartInstance[channel].p_reg, NRF_UART_EVENT_ERROR);
-        nrf_uart_event_clear(_uartInstance[channel].p_reg, NRF_UART_EVENT_RXDRDY);
-        nrf_uart_int_enable(_uartInstance[channel].p_reg, NRF_UART_INT_MASK_TXDRDY | NRF_UART_INT_MASK_RXTO);
-        nrf_uart_int_enable(_uartInstance[channel].p_reg, NRF_UART_INT_MASK_RXDRDY | NRF_UART_INT_MASK_ERROR);
+        nrf_uarte_int_enable(_uartInstance[channel].p_reg,
+                             NRF_UARTE_INT_ENDRX_MASK |
+                                 NRF_UARTE_INT_ENDTX_MASK |
+                                 NRF_UARTE_INT_TXSTOPPED_MASK);
+
         NRFX_IRQ_PRIORITY_SET(nrfx_get_irq_number((void*)_uartInstance[channel].p_reg), IRQ_PRIORITY_UART);
+        NVIC_ClearPendingIRQ(nrfx_get_irq_number((void*)_uartInstance[channel].p_reg));
         NRFX_IRQ_ENABLE(nrfx_get_irq_number((void*)_uartInstance[channel].p_reg));
 
-        nrf_uart_task_trigger(_uartInstance[channel].p_reg, NRF_UART_TASK_STARTRX);
-        nrf_uart_enable(_uartInstance[channel].p_reg);
+        nrf_uarte_rx_buffer_set(_uartInstance[channel].p_reg, &_nrfRxBuffer[channel], 1);
+        nrf_uarte_enable(_uartInstance[channel].p_reg);
+        nrf_uarte_task_trigger(_uartInstance[channel].p_reg, NRF_UARTE_TASK_STARTRX);
 
         return true;
     }
@@ -224,40 +257,37 @@ namespace Board::detail::UART::MCU
 
 void core::mcu::isr::uart(uint8_t channel)
 {
-    if (nrf_uart_int_enable_check(_uartInstance[channel].p_reg, NRF_UART_INT_MASK_ERROR) && nrf_uart_event_check(_uartInstance[channel].p_reg, NRF_UART_EVENT_ERROR))
+    if (nrf_uarte_event_check(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_ERROR))
     {
-        nrf_uart_event_clear(_uartInstance[channel].p_reg, NRF_UART_EVENT_ERROR);
-        nrf_uart_int_disable(_uartInstance[channel].p_reg, NRF_UART_INT_MASK_RXDRDY | NRF_UART_INT_MASK_ERROR);
-        nrf_uart_errorsrc_get_and_clear(_uartInstance[channel].p_reg);
+        nrf_uarte_event_clear(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_ERROR);
+        nrf_uarte_errorsrc_get_and_clear(_uartInstance[channel].p_reg);
     }
-    else if (nrf_uart_int_enable_check(_uartInstance[channel].p_reg, NRF_UART_INT_MASK_RXDRDY) && nrf_uart_event_check(_uartInstance[channel].p_reg, NRF_UART_EVENT_RXDRDY))
+    else if (nrf_uarte_event_check(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_ENDRX))
     {
-        nrf_uart_event_clear(_uartInstance[channel].p_reg, NRF_UART_EVENT_RXDRDY);
-        Board::detail::UART::storeIncomingData(channel, nrf_uart_rxd_get(_uartInstance[channel].p_reg));
-    }
+        // receive buffer is filled up
 
-    if (nrf_uart_event_check(_uartInstance[channel].p_reg, NRF_UART_EVENT_TXDRDY))
-    {
-        nrf_uart_event_clear(_uartInstance[channel].p_reg, NRF_UART_EVENT_TXDRDY);
-
-        size_t  remainingBytes;
-        uint8_t value;
-
-        if (Board::detail::UART::getNextByteToSend(channel, value, remainingBytes))
-        {
-            nrf_uart_txd_set(_uartInstance[channel].p_reg, value);
-        }
-        else
-        {
-            nrf_uart_task_trigger(_uartInstance[channel].p_reg, NRF_UART_TASK_STOPTX);
-            _transmitting[channel] = false;
-        }
+        nrf_uarte_event_clear(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_ENDRX);
+        Board::detail::UART::storeIncomingData(channel, _nrfRxBuffer[channel]);
+        nrf_uarte_task_trigger(_uartInstance[channel].p_reg, NRF_UARTE_TASK_STARTRX);
     }
 
-    if (nrf_uart_event_check(_uartInstance[channel].p_reg, NRF_UART_EVENT_RXTO))
+    // Receiver timeout
+    if (nrf_uarte_event_check(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_RXTO))
     {
-        nrf_uart_event_clear(_uartInstance[channel].p_reg, NRF_UART_EVENT_RXTO);
-        nrf_uart_task_trigger(_uartInstance[channel].p_reg, NRF_UART_TASK_STARTRX);
+        nrf_uarte_event_clear(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_RXTO);
+    }
+
+    //  last TX byte transmitted from internal NRF buffer
+    if (nrf_uarte_event_check(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_ENDTX))
+    {
+        nrf_uarte_event_clear(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_ENDTX);
+        checkTx(channel);
+    }
+
+    // transmitting stopped
+    if (nrf_uarte_event_check(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_TXSTOPPED))
+    {
+        nrf_uarte_event_clear(_uartInstance[channel].p_reg, NRF_UARTE_EVENT_TXSTOPPED);
     }
 }
 
--- src/board/arch/arm/nordic/variants/nrf52/common/sdk_config.h
+++ src/board/arch/arm/nordic/variants/nrf52/common/sdk_config.h
@@ -4286,12 +4286,12 @@
 #endif
 // <o> NRFX_UARTE0_ENABLED - Enable UARTE0 instance
 #ifndef NRFX_UARTE0_ENABLED
-#define NRFX_UARTE0_ENABLED 0
+#define NRFX_UARTE0_ENABLED 1
 #endif
 
 // <o> NRFX_UARTE1_ENABLED - Enable UARTE1 instance
 #ifndef NRFX_UARTE1_ENABLED
-#define NRFX_UARTE1_ENABLED 0
+#define NRFX_UARTE1_ENABLED 1
 #endif
 
 // <o> NRFX_UARTE_DEFAULT_CONFIG_HWFC  - Hardware Flow Control
