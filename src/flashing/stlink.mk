fd541fbf3850c7ba21974dbbeab74cd3124ad42e
paradajz <2544094+paradajz@users.noreply.github.com>
2022-06-22 15:50:41 +0000

    build: rename STLINK_SERIAL to PROBE_ID

--- src/flashing/stlink.mk
+++ src/flashing/stlink.mk
@@ -1,4 +1,4 @@
 # STM32 Programmer utility on SWD interface
 
 flash:
-	@STM32_Programmer_CLI -c port=SWD sn=$(STLINK_SERIAL) -w $(FLASH_BINARY_DIR)/$(TARGET).hex -rst
+	@STM32_Programmer_CLI -c port=SWD sn=$(PROBE_ID) -w $(FLASH_BINARY_DIR)/$(TARGET).hex -rst
