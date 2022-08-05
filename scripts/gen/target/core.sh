e0373cf65b96c945ffdcfc4e33ab1d292df564d2
paradajz <2544094+paradajz@users.noreply.github.com>
2022-08-05 12:50:03 +0200

    build: specify ext mcu clock in target makefile instead of mcu

--- scripts/gen/target/core.sh
+++ scripts/gen/target/core.sh
@@ -14,13 +14,6 @@ then
     exit 1
 fi
 
-extClockMhz=$($yaml_parser "$yaml_file" extClockMhz)
-
-if [[ $extClockMhz == "null" ]]
-then
-    extClockMhz=""
-fi
-
 if [[ ! -d $mcu_gen_dir ]]
 then
     core_mcu_config_path=$(find "$script_dir"/../../modules/core/src -type f -name "*$mcu.yml")
@@ -44,6 +37,22 @@ fi
 
 {
     printf "%s%s\n" '-include $(MAKEFILE_INCLUDE_PREFIX)$(BOARD_GEN_DIR_MCU_BASE)/' "$mcu/MCU.mk"
+} >> "$out_makefile"
+
+# Normally, ext clock freq is generated in MCU.mk file by core module, however, various OpenDeck boards
+# can use the same MCU with different clock speed. Therefore, define this in target makefile.
+extClockMhz=$($yaml_parser "$yaml_file" extClockMhz)
+
+if [[ "$extClockMhz" != "null" ]]
+then
+    printf "%s\n" "DEFINES += CORE_MCU_EXT_CLOCK_FREQ_MHZ=$extClockMhz" >> "$out_makefile"
+fi
+
+# If CORE_MCU_CPU_FREQ_MHZ symbol isn't defined, use external clock frequency as core frequency
+{
+    printf "%s\n" 'ifeq (,$(findstring CORE_MCU_CPU_FREQ_MHZ,$(DEFINES)))'
+    printf "%s\n" "DEFINES += CORE_MCU_CPU_FREQ_MHZ=$extClockMhz"
+    printf "%s\n" "endif"
 } >> "$out_makefile"
 
 board_name=$($yaml_parser "$yaml_file" boardNameOverride)
