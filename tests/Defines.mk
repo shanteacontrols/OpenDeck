dd1ff054676f4bff44038bce8603de30421ec4fa
paradajz <2544094+paradajz@users.noreply.github.com>
2022-10-31 12:27:58 +0100

    update core

--- tests/Defines.mk
+++ tests/Defines.mk
@@ -5,9 +5,7 @@ GLOG_CUSTOM_PREFIX_SUPPORT
 
 TEST_DEFINES := 1
 
-# Filter out arch symbols to avoid pulling MCU-specific headers
-DEFINES := $(filter-out CORE_ARCH_%,$(DEFINES))
-DEFINES := $(filter-out CORE_VENDOR_%,$(DEFINES))
+DEFINES += CORE_MCU_STUB
 DEFINES := $(filter-out DATABASE_INIT_DATA%,$(DEFINES))
 
 DEFINES += DATABASE_INIT_DATA=1
