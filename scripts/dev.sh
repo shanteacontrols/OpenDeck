3beaa157b3e367ede6693005e606fac6f19afe8b
paradajz <2544094+paradajz@users.noreply.github.com>
2022-11-20 20:30:32 +0100

    docker: improve dev.sh script
    allow running dev.sh script with --build flag for testing local images

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,22 +2,32 @@
 
 set -e
 
-project="opendeck"
-service="opendeck"
+compose_file=docker-compose.yml
 
-if [[ $(docker ps | grep "${project}_${service}") != "" ]]
+for arg in "$@"; do
+    case "$arg" in
+        --build)
+            compose_file=docker-compose-build.yml
+            ;;
+    esac
+done
+
+# Avoid using dasel or other YAML parsers here not to introduce
+# dependencies for starting a container.
+container=$(cat $compose_file | grep container_name | xargs | cut -d: -f2 | xargs)
+
+if [[ $(docker ps | grep "${container}") != "" ]]
 then
     echo "Found the following containers already online:"
-    docker ps -f name="${project}"_"${service}"
+    docker ps -f name="${container}"
 
     printf "\n%s" "Specify the container ID to attach to: "
     read -r container
     echo "Attaching to $container..."
 else
     echo "Creating new container"
-    docker-compose build
-    docker-compose -f docker-compose.yml up -d
-    container=${project}_${service}_1
+    docker-compose -f $compose_file build
+    docker-compose -f $compose_file up -d
 fi
 
 docker exec -it "$container" bash
