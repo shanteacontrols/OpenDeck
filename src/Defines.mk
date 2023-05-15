25981ae7d4413610ee3335277301598c5283a014
paradajz <2544094+paradajz@users.noreply.github.com>
2023-05-15 20:49:57 +0200

    build: don't use global logger size

--- src/Defines.mk
+++ src/Defines.mk
@@ -3,11 +3,9 @@ SW_VERSION_MINOR            := $(shell git describe --tags --abbrev=0 | cut -c 2
 SW_VERSION_REVISION         := $(shell git describe --tags --abbrev=0 | cut -c 2- | cut -d. -f3)
 
 MIDI_SYSEX_ARRAY_SIZE       ?= 100
-LOGGER_BUFFER_SIZE          := 128
 
 DEFINES += \
 MIDI_SYSEX_ARRAY_SIZE=$(MIDI_SYSEX_ARRAY_SIZE) \
-LOGGER_BUFFER_SIZE=$(LOGGER_BUFFER_SIZE) \
 SW_VERSION_MAJOR=$(SW_VERSION_MAJOR) \
 SW_VERSION_MINOR=$(SW_VERSION_MINOR) \
 SW_VERSION_REVISION=$(SW_VERSION_REVISION)
