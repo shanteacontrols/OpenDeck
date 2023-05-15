a7dc0ea7223d42d2740a0f1fcea9f5451d086889
paradajz <2544094+paradajz@users.noreply.github.com>
2023-05-15 21:17:51 +0200

    update midi module

--- src/Defines.mk
+++ src/Defines.mk
@@ -2,10 +2,7 @@ SW_VERSION_MAJOR            := $(shell git describe --tags --abbrev=0 | cut -c 2
 SW_VERSION_MINOR            := $(shell git describe --tags --abbrev=0 | cut -c 2- | cut -d. -f2)
 SW_VERSION_REVISION         := $(shell git describe --tags --abbrev=0 | cut -c 2- | cut -d. -f3)
 
-MIDI_SYSEX_ARRAY_SIZE       ?= 100
-
 DEFINES += \
-MIDI_SYSEX_ARRAY_SIZE=$(MIDI_SYSEX_ARRAY_SIZE) \
 SW_VERSION_MAJOR=$(SW_VERSION_MAJOR) \
 SW_VERSION_MINOR=$(SW_VERSION_MINOR) \
 SW_VERSION_REVISION=$(SW_VERSION_REVISION)
