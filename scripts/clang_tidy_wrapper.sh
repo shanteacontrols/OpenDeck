de0e5c14f06a2d3aab11f07870d4d71920ff4090
paradajz <2544094+paradajz@users.noreply.github.com>
2023-05-31 16:16:56 +0200

    clang_tidy_wrapper.sh: use consistent spacing

--- scripts/clang_tidy_wrapper.sh
+++ scripts/clang_tidy_wrapper.sh
@@ -31,8 +31,8 @@ done
 
 if [[ -z $output_file ]]
 then
-  echo "Output file not specified"
-  exit 1
+    echo "Output file not specified"
+    exit 1
 fi
 
 extra_args=()
@@ -52,55 +52,55 @@ extra_args+=(-extra-arg="-include$script_dir/../src/generated/mcu/$mcu/CoreMCUGe
 extra_args+=(-extra-arg="-DCORE_MCU_STUB")
 
 case $compiler in
-  arm-none-eabi-gcc)
-    toolchain_dir=$(dirname "$(which arm-none-eabi-gcc)" | rev | cut -d/ -f2- | rev)
-    toolchain_ver=$(find "$toolchain_dir"/arm-none-eabi/include/c++ -mindepth 1 -maxdepth 1 | rev | cut -d/ -f 1 | rev)
-    extra_args+=(-extra-arg="-I$toolchain_dir/arm-none-eabi/include/c++/$toolchain_ver")
-    extra_args+=(-extra-arg="-I$toolchain_dir/arm-none-eabi/include/c++/$toolchain_ver/arm-none-eabi")
-    extra_args+=(-extra-arg="-I$toolchain_dir/arm-none-eabi/include/c++/$toolchain_ver/backward")
-    extra_args+=(-extra-arg="-I$toolchain_dir/lib/gcc/arm-none-eabi/$toolchain_ver/include")
-    extra_args+=(-extra-arg="-I$toolchain_dir/lib/gcc/arm-none-eabi/$toolchain_ver/include-fixed")
-    extra_args+=(-extra-arg="-I$toolchain_dir/arm-none-eabi/sys-include")
-    extra_args+=(-extra-arg="-I$toolchain_dir/arm-none-eabi/include")
-    extra_args+=(-extra-arg="-Wno-unknown-attributes")
+    arm-none-eabi-gcc)
+      toolchain_dir=$(dirname "$(which arm-none-eabi-gcc)" | rev | cut -d/ -f2- | rev)
+      toolchain_ver=$(find "$toolchain_dir"/arm-none-eabi/include/c++ -mindepth 1 -maxdepth 1 | rev | cut -d/ -f 1 | rev)
+      extra_args+=(-extra-arg="-I$toolchain_dir/arm-none-eabi/include/c++/$toolchain_ver")
+      extra_args+=(-extra-arg="-I$toolchain_dir/arm-none-eabi/include/c++/$toolchain_ver/arm-none-eabi")
+      extra_args+=(-extra-arg="-I$toolchain_dir/arm-none-eabi/include/c++/$toolchain_ver/backward")
+      extra_args+=(-extra-arg="-I$toolchain_dir/lib/gcc/arm-none-eabi/$toolchain_ver/include")
+      extra_args+=(-extra-arg="-I$toolchain_dir/lib/gcc/arm-none-eabi/$toolchain_ver/include-fixed")
+      extra_args+=(-extra-arg="-I$toolchain_dir/arm-none-eabi/sys-include")
+      extra_args+=(-extra-arg="-I$toolchain_dir/arm-none-eabi/include")
+      extra_args+=(-extra-arg="-Wno-unknown-attributes")
     ;;
 
-  avr-gcc)
-    toolchain_dir=$(dirname "$(which avr-gcc)" | rev | cut -d/ -f2- | rev)
-    extra_args+=(-extra-arg="-I$toolchain_dir/avr/include/avr")
-    extra_args+=(-extra-arg="-I$toolchain_dir/avr/include")
-    extra_args+=(-extra-arg="-I$toolchain_dir/lib/gcc/avr/7.3.0/include")
-    extra_args+=(-extra-arg="-Wno-unknown-attributes")
-    extra_args+=(-extra-arg="--target=avr")
+    avr-gcc)
+      toolchain_dir=$(dirname "$(which avr-gcc)" | rev | cut -d/ -f2- | rev)
+      extra_args+=(-extra-arg="-I$toolchain_dir/avr/include/avr")
+      extra_args+=(-extra-arg="-I$toolchain_dir/avr/include")
+      extra_args+=(-extra-arg="-I$toolchain_dir/lib/gcc/avr/7.3.0/include")
+      extra_args+=(-extra-arg="-Wno-unknown-attributes")
+      extra_args+=(-extra-arg="--target=avr")
     ;;
 
-  *)
-    # nothing to add
+    *)
+      # nothing to add
     ;;
 esac
 
 if [[ -n $option_files_dir ]]
 then
-  readarray -t ignore_paths <"$option_files_dir"/.clang-tidy-ignore
-  ignore_regex="^((?!"
+    readarray -t ignore_paths <"$option_files_dir"/.clang-tidy-ignore
+    ignore_regex="^((?!"
 
-  for line in "${ignore_paths[@]}"
-  do
-    ignore_regex+=$line
-    ignore_regex+="|"
-  done
+    for line in "${ignore_paths[@]}"
+    do
+      ignore_regex+=$line
+      ignore_regex+="|"
+    done
 
-  # Last '|' must be removed
-  ignore_regex=${ignore_regex::-1}
-  ignore_regex+=").)*$"
+    # Last '|' must be removed
+    ignore_regex=${ignore_regex::-1}
+    ignore_regex+=").)*$"
 
-  # Clean up compile_commands.json
-  readarray -t cleanup_args <"$option_files_dir"/.clang-tidy-compile-commands-clean
+    # Clean up compile_commands.json
+    readarray -t cleanup_args <"$option_files_dir"/.clang-tidy-compile-commands-clean
 
-  for line in "${cleanup_args[@]}"
-  do
-      sed -i "s/$line//g" compile_commands.json
-  done
+    for line in "${cleanup_args[@]}"
+    do
+        sed -i "s/$line//g" compile_commands.json
+    done
 fi
 
 run-clang-tidy \
