33d0c3e65a6283eb49268b0faaab066c728ef792
paradajz <2544094+paradajz@users.noreply.github.com>
2023-05-31 16:18:20 +0200

    dockerfile: remove infer

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -40,38 +40,11 @@ clang_tidy_lint()
     cd ../
 }
 
-infer_lint()
-{
-    echo "Linting test code with infer for target: $1"
-    dir=tests
-    cd $dir
-    build_cmd="make TARGET=$1 DEBUG=0"
-    eval "$build_cmd" clean
-    eval "$build_cmd" generate
-    eval "$build_cmd" lint
-    cd ../
-}
-
 clang_tidy_lint discovery
-infer_lint discovery
-
 clang_tidy_lint opendeck2
-infer_lint opendeck2
-
 clang_tidy_lint rooibos
-infer_lint rooibos
-
 clang_tidy_lint nrf52840dk
-infer_lint nrf52840dk
-
 clang_tidy_lint mega2560 0
-infer_lint mega2560
-
 clang_tidy_lint mega16u2 0
-infer_lint mega16u2
-
 clang_tidy_lint opendeck_mini 0
-infer_lint opendeck_mini
-
 clang_tidy_lint teensy2pp 0
-infer_lint teensy2pp
