c07eb225c6dc3c8d609802a986886ce5ae42c855
paradajz <2544094+paradajz@users.noreply.github.com>
2023-06-10 19:42:52 +0200

    build: simplify adding modules

--- cmake/include/helpers.cmake
+++ cmake/include/helpers.cmake
@@ -8,3 +8,26 @@ function(check_required_executables)
         endif()
     endforeach()
 endfunction()
+
+# Add a specified module to the build
+function(add_module module_name)
+    add_subdirectory(${PROJECT_ROOT}/modules/${module_name} ${CMAKE_CURRENT_BINARY_DIR}/modules/${module_name})
+endfunction()
+
+# Link and compile specified library with options for the mcu target
+macro(add_mcu_library library_name)
+    target_include_directories(${library_name}
+        PRIVATE
+        "$<TARGET_PROPERTY:mcu,INCLUDE_DIRECTORIES>"
+    )
+
+    target_compile_options(${library_name}
+        PUBLIC
+        "$<TARGET_PROPERTY:mcu,COMPILE_OPTIONS>"
+    )
+
+    target_link_options(${library_name}
+        PUBLIC
+        "$<TARGET_PROPERTY:mcu,LINK_OPTIONS>"
+    )
+endmacro()
