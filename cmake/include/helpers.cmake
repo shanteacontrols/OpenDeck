17ca37a6c1084dbb4af0089623a9db7f2c2306d1
paradajz <2544094+paradajz@users.noreply.github.com>
2024-02-23 12:15:15 +0000

    build/git: replace git submodules with west tool

--- cmake/include/helpers.cmake
+++ cmake/include/helpers.cmake
@@ -11,7 +11,7 @@ endfunction()
 
 # Add a specified module to the build
 function(add_module module_name)
-    add_subdirectory(${PROJECT_ROOT}/modules/${module_name} ${CMAKE_CURRENT_BINARY_DIR}/modules/${module_name})
+    add_subdirectory(${WORKSPACE_ROOT}/modules/${module_name} ${CMAKE_CURRENT_BINARY_DIR}/modules/${module_name})
 endfunction()
 
 # Link and compile specified library with options for the mcu target
