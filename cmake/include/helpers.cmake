8e364db03c096462044fa3de1d797b9e6b99b439
paradajz <2544094+paradajz@users.noreply.github.com>
2024-02-24 09:53:29 +0100

    build: remove unnecessary cmake check

--- cmake/include/helpers.cmake
+++ cmake/include/helpers.cmake
@@ -1,14 +1,3 @@
-function(check_required_executables)
-    foreach(executable ${ARGV})
-        find_program(${executable}_result ${executable})
-        if(${executable}_result)
-            message(STATUS "Required executable ${executable} found at: ${${executable}_result}")
-        else()
-            message(FATAL_ERROR "Required executable ${executable} not found!")
-        endif()
-    endforeach()
-endfunction()
-
 # Add a specified module to the build
 function(add_module module_name)
     add_subdirectory(${WORKSPACE_ROOT}/modules/${module_name} ${CMAKE_CURRENT_BINARY_DIR}/modules/${module_name})
