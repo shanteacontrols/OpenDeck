e1cca8ea496adc6d6876e72e08461f251ec14c0f
paradajz <2544094+paradajz@users.noreply.github.com>
2026-06-02 10:33:13 +0000

    cmake: add the option to specify lib type in opendeck_library

--- cmake/opendeck_library.cmake
+++ cmake/opendeck_library.cmake
@@ -1,12 +1,51 @@
 macro(opendeck_library_named name)
-    set(ZEPHYR_CURRENT_LIBRARY ${name})
-    add_library(${name} STATIC "")
-    target_link_libraries(${name} PUBLIC zephyr_interface)
+    unset(OPENDECK_LIBRARY_TYPE)
+    unset(OPENDECK_LIBRARY_UNPARSED_ARGUMENTS)
+
+    cmake_parse_arguments(OPENDECK_LIBRARY "" "TYPE" "" ${ARGN})
+
+    if(OPENDECK_LIBRARY_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unexpected arguments for ${name}: ${OPENDECK_LIBRARY_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(NOT OPENDECK_LIBRARY_TYPE)
+        set(OPENDECK_LIBRARY_TYPE STATIC)
+    endif()
+
+    if(OPENDECK_LIBRARY_TYPE STREQUAL INTERFACE)
+        add_library(${name} INTERFACE)
+        target_link_libraries(${name} INTERFACE zephyr_interface)
+    else()
+        set(ZEPHYR_CURRENT_LIBRARY ${name})
+        add_library(${name} ${OPENDECK_LIBRARY_TYPE} "")
+        target_link_libraries(${name} PUBLIC zephyr_interface)
+    endif()
+
     add_dependencies(${name} zephyr_generated_headers)
 endmacro()
 
 macro(opendeck_interface_library_named name)
-    add_library(${name} INTERFACE)
-    target_link_libraries(${name} INTERFACE zephyr_interface)
+    unset(OPENDECK_LIBRARY_TYPE)
+    unset(OPENDECK_LIBRARY_UNPARSED_ARGUMENTS)
+
+    cmake_parse_arguments(OPENDECK_LIBRARY "" "TYPE" "" ${ARGN})
+
+    if(OPENDECK_LIBRARY_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unexpected arguments for ${name}: ${OPENDECK_LIBRARY_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(NOT OPENDECK_LIBRARY_TYPE)
+        set(OPENDECK_LIBRARY_TYPE INTERFACE)
+    endif()
+
+    if(OPENDECK_LIBRARY_TYPE STREQUAL INTERFACE)
+        add_library(${name} INTERFACE)
+        target_link_libraries(${name} INTERFACE zephyr_interface)
+    else()
+        set(ZEPHYR_CURRENT_LIBRARY ${name})
+        add_library(${name} ${OPENDECK_LIBRARY_TYPE} "")
+        target_link_libraries(${name} PUBLIC zephyr_interface)
+    endif()
+
     add_dependencies(${name} zephyr_generated_headers)
 endmacro()
